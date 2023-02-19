120af56c9a969cd557ca6f327fa602198b0c00c5
disservin <45608332+Disservin@users.noreply.github.com>
2023-02-19 10:53:21 +0100

    uci files

---
+++ src/uci.h
@@ -0,0 +1,20 @@
+class Uci
+{
+  public:
+    Uci();
+    Uci(Uci &&) = default;
+    Uci(const Uci &) = default;
+    Uci &operator=(Uci &&) = default;
+    Uci &operator=(const Uci &) = default;
+    ~Uci();
+
+  private:
+};
+
+Uci::Uci()
+{
+}
+
+Uci::~Uci()
+{
+}

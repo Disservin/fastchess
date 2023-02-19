142d5624c93dd5e5aece4ec4078c50ce3d6feba4
disservin <45608332+Disservin@users.noreply.github.com>
2023-02-19 12:23:05 +0100

    fix exit code

--- tests/main.cpp
+++ tests/main.cpp
@@ -13,11 +13,12 @@ int main(int argc, char **argv)
     doctest::Context ctx;
 
     ctx.setOption("abort-after", 5); // default - stop after 5 failed asserts
+    ctx.setOption("no-exitcode", false);
 
     int res = ctx.run(); // run test cases unless with --no-run
 
     if (ctx.shouldExit()) // query flags (and --exit) rely on this
         return res;       // propagate the result of the tests
 
-    return 0;
+    return res;
 }

917cbccce9ac9295354abdbd09e61014538c9de0
PGG106 <g.pagano45@studenti.unisa.it>
2023-02-19 11:44:48 +0100

    Squashed commit of the following:
    commit cd87fe8239360fa370a955e3bc2da651b4dba1f2
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sun Feb 19 11:36:05 2023 +0100
    
        finish engine parsing
    
    commit 328765c92dae79fd84702afd7a5e436a0353e295
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sun Feb 19 11:29:31 2023 +0100
    
        tweaks
    
    commit d7fe7ed9bd96297117fef48d8f15d47b62ae3e29
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sun Feb 19 10:57:59 2023 +0100
    
        time control parsing
    
    commit 040601cb75ca490d433476c14486e330cf8e653a
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 15:38:29 2023 +0100
    
        partial tc parsing
    
    commit a50e13e8ca3eec454e882e6592b3340d95b7dd6a
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 15:22:07 2023 +0100
    
        introduce game managar options structure, more stubs
    
    commit a534c9c77942200c880067395fadee60f2ce5015
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 09:51:16 2023 +0100
    
        don't break things thx
    
    commit 960de764788ff591e1e7f765267e3ed2cc2ba025
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 09:50:59 2023 +0100
    
        rename Parameter struct
    
    commit 6a6440bea48d1175cfd4ad40dd321bb98559a38f
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 09:15:12 2023 +0100
    
        improve parsing stub
    
    commit bcd2122fa96b97bcaeec54a6f69e189539cec749
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 09:04:20 2023 +0100
    
        parameter parsing stub
    
    commit f5283f887932a376b48c56834cbf41fe3f1dac38
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 08:11:36 2023 +0100
    
        change splitInput into more generic splitString
    
    commit c87321b6625bb39356457a2c12dc49187aa35517
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 08:11:17 2023 +0100
    
        change splitInput into more generic splitString
    
    commit 67ad1646532ce119e8fc9aed5796bac9e840bb96
    Author: PGG106 <g.pagano45@studenti.unisa.it>
    Date:   Sat Feb 18 07:27:28 2023 +0100
    
        refactor option parsing

--- src/engine.h
+++ src/engine.h
@@ -8,54 +8,54 @@
 
 struct TimeControl
 {
-    uint64_t moves;
-    uint64_t time;
-    uint64_t increment;
+  uint64_t moves;
+  uint64_t time;
+  uint64_t increment;
 };
 
 class Engine
 {
-  private:
-    // engine name
-    std::string name;
+private:
+  // engine name
+  std::string name;
 
-    // Path to engine
-    std::string cmd;
+  // Path to engine
+  std::string cmd;
 
-    // Custom args that should be sent
-    std::string args;
+  // Custom args that should be sent
+  std::string args;
 
-    // UCI options
-    std::vector<std::string> options;
+  // UCI options
+  std::vector<std::string> options;
 
-    // time control for the engine
-    TimeControl tc;
+  // time control for the engine
+  TimeControl tc;
 
-    // Process wrapper around the engine
-    EngineProcess process;
+  // Process wrapper around the engine
+  EngineProcess process;
 
-  public:
-    Engine() = default;
-    Engine(const std::string &command);
-    ~Engine();
+public:
+  Engine() = default;
+  Engine(const std::string &command);
+  ~Engine();
 
-    Engine setName(const std::string &name);
-    Engine setArgs(const std::string &args);
-    Engine setOptions(const std::vector<std::string> &options);
-    Engine setTc(const TimeControl &tc);
-    Engine setCmd(const std::string &command);
+  Engine setName(const std::string &name);
+  Engine setArgs(const std::string &args);
+  Engine setOptions(const std::vector<std::string> &options);
+  Engine setTc(const TimeControl &tc);
+  Engine setCmd(const std::string &command);
 
-    void startProcess();
-    void stopProcess();
-    void pingProcess();
+  void startProcess();
+  void stopProcess();
+  void pingProcess();
 
-    void writeProcess(const std::string &input);
-    std::vector<std::string> readProcess(const std::string &last_word, int64_t timeoutThreshold = 1000);
-    std::vector<std::string> readProcess(const std::string &last_word, bool &timedOut, int64_t timeoutThreshold = 1000);
+  void writeProcess(const std::string &input);
+  std::vector<std::string> readProcess(const std::string &last_word, int64_t timeoutThreshold = 1000);
+  std::vector<std::string> readProcess(const std::string &last_word, bool &timedOut, int64_t timeoutThreshold = 1000);
 
-    std::string getName() const;
-    std::string getCmd() const;
-    std::string getArgs() const;
-    std::vector<std::string> getOptions() const;
-    TimeControl getTc() const;
+  std::string getName() const;
+  std::string getCmd() const;
+  std::string getArgs() const;
+  std::vector<std::pair<std::string, std::string>> getOptions() const;
+  TimeControl getTc() const;
 };

420dcc4bd20e0291c2598aec65b73c3fbe9f44b5
PGG106 <g.pagano45@studenti.unisa.it>
2023-02-19 16:50:25 +0100

    restore old main

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,19 @@
 int main(int argc, char const *argv[])
 {
     CMD::Options options = CMD::Options(argc, argv);
+    options.print_params();
+
+    Engine engine("./DummyEngine");
+
+    engine.writeProcess("uci");
+    auto output = engine.readProcess("uciok");
+
+    for (const auto &item : output)
+    {
+        std::cout << item << std::endl;
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     return 0;
 }

06356f3c4d1c48e7a8db7b8557168fc35435a91c
PGG106 <g.pagano45@studenti.unisa.it>
2023-02-20 12:33:08 +0100

    fix expand and improve option parsing test

--- tests/options_test.h
+++ tests/options_test.h
@@ -3,13 +3,14 @@
 
 #include <cassert>
 
-TEST_CASE("Testing options")
+TEST_CASE("Testing Engine options parsing")
 {
     const char *argv[] = {"fast-chess.exe",
-                          "-engine dir=Engines/",
+                          "-engine",
+                          "dir=Engines/",
                           "cmd=./Alexandria-EA649FED.exe",
-                          "proto=uci"
-                          "tc=9.64+0.10",
+                          "proto=uci",
+                          "tc=10/9.64",
                           "option.Threads=1",
                           "option.Hash=16",
                           "name=Alexandria-EA649FED",
@@ -19,7 +20,7 @@ TEST_CASE("Testing options")
                           "proto=uci",
                           "tc=9.64+0.10",
                           "option.Threads=1",
-                          "option.Hash=16",
+                          "option.Hash=32",
                           "name=Alexandria-27E42728",
                           "-openings",
                           "file=Books/Pohl.epd",
@@ -29,11 +30,25 @@ TEST_CASE("Testing options")
                           "-pgnout",
                           "PGNs/Alexandria-EA649FED_vs_Alexandria-27E42728"};
     CMD::Options options = CMD::Options(22, argv);
-
-    CHECK(options.configs[0].name == "Alexandria-27E42728");
-    CHECK(options.configs[0].tc.moves == 0);
+    CHECK(options.configs.size() == 2);
+    CHECK(options.configs[0].name == "Alexandria-EA649FED");
+    CHECK(options.configs[0].tc.moves == 10);
     CHECK(options.configs[0].tc.time == 9640);
-    CHECK(options.configs[0].tc.increment == 100);
+    CHECK(options.configs[0].tc.increment == 0);
     CHECK(options.configs[0].nodes == 0);
     CHECK(options.configs[0].plies == 0);
+    CHECK(options.configs[0].options.at(0).first == "option.Threads");
+    CHECK(options.configs[0].options.at(0).second == "1");
+    CHECK(options.configs[0].options.at(1).first == "option.Hash");
+    CHECK(options.configs[0].options.at(1).second == "16");
+    CHECK(options.configs[1].name == "Alexandria-27E42728");
+    CHECK(options.configs[1].tc.moves == 0);
+    CHECK(options.configs[1].tc.time == 9640);
+    CHECK(options.configs[1].tc.increment == 100);
+    CHECK(options.configs[1].nodes == 0);
+    CHECK(options.configs[1].plies == 0);
+    CHECK(options.configs[1].options.at(0).first == "option.Threads");
+    CHECK(options.configs[1].options.at(0).second == "1");
+    CHECK(options.configs[1].options.at(1).first == "option.Hash");
+    CHECK(options.configs[1].options.at(1).second == "32");
 }

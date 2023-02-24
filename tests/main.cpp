4e3bcc038196ffcd7b49dc104d205e8478324b6a
disservin <45608332+Disservin@users.noreply.github.com>
2023-02-24 16:16:39 +0100

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,16 +1,16 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
 
-// #include "elo_test.h"
-// #include "engine_test.h"
-// #include "fen_repetition_test.h"
-// #include "functions_test.h"
-// #include "hash_test.h"
-// #include "options_test.h"
-// #include "perft_test.h"
-// #include "san_test.h"
+#include "elo_test.h"
+#include "engine_test.h"
+#include "fen_repetition_test.h"
+#include "functions_test.h"
+#include "hash_test.h"
+#include "options_test.h"
+#include "perft_test.h"
+#include "san_test.h"
 #include "tournament_test.h"
-// #include "uci_engine_test.h"
+#include "uci_engine_test.h"
 
 int main(int argc, char **argv)
 {

ab15f0f41733eeefa04e2c38435e353b0fa58604
disservin <45608332+Disservin@users.noreply.github.com>
2023-02-25 09:43:43 +0100

    fix tests

--- tests/tournament_test.h
+++ tests/tournament_test.h
@@ -23,9 +23,10 @@ TEST_CASE("Test Tournament")
 
     tour.startTournament({engine, engine});
 
-    std::string pgn = "[Event \"\"]\n[Site \"?\"]\n[Date \"\"]\n[Round \"0\"]\n[White \"\"]\n[Black \"\"]\n[Result "
-                      "\"0-1\"]\n[FEN \"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1\"]\n[GameDuration "
-                      "\"\"]\n[GameEndTime \"\"]\n[GameStartTime \"\"]\n[PlyCount \"4\"]\n[TimeControl \"0\"]";
+    std::string pgn =
+        "[Event \"\"]\n[Site \"?\"]\n[Date \"\"]\n[Round \"0\"]\n[White \"\"]\n[Black \"\"]\n[Result \"0-1\"]\n[FEN "
+        "\"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1\"]\n[GameDuration \"\"]\n[GameEndTime "
+        "\"\"]\n[GameStartTime \"\"]\n[PlyCount \"4\"]\n[TimeControl \"0\"]\n\n1. f3 e5 2. g4 Qh4#  0-1\n";
 
     CHECK(tour.getPGNS()[0] == pgn);
 }

0261b85789779dff7c08d7a9abed525240185f37
PGG106 <g.pagano45@studenti.unisa.it>
2023-03-03 14:39:24 +0100

    fix the stat calculation for games != 2

--- src/tournament.cpp
+++ src/tournament.cpp
@@ -95,7 +95,7 @@ void Tournament::printElo()
     ss << "---------------------------\n"
        << "Result of " << engineNames[0] << " vs " << engineNames[1]
        << ": " << wins << " - " << losses << " - " << draws
-       << " (" << std::fixed << std::setprecision(2) << (float(wins) + (float(draws) * 0.5)) / (roundCount * 2) << ")\n"
+       << " (" << std::fixed << std::setprecision(2) << (float(wins) + (float(draws) * 0.5)) / (roundCount * this->matchConfig.games) << ")\n"
        << "Ptnml:   "
        << std::right << std::setw(7) << "WW"
        << std::right << std::setw(7) << "WD"
@@ -115,9 +115,9 @@ void Tournament::printElo()
         ss << "LLR: " << sprt.getLLR(wins, draws, losses) << " " << sprt.getBounds() << "\n";
     }
     ss << "Games:" << roundCount * 2 << std::setprecision(1)
-       << " W:" << (float(wins) / (roundCount * 2)) * 100 << "% "
-       << "L:" << (float(losses) / (roundCount * 2)) * 100 << "% "
-       << "D:" << (float(draws) / (roundCount * 2)) * 100 << "%\n";
+       << " W:" << (float(wins) / (roundCount * this->matchConfig.games)) * 100 << "% "
+       << "L:" << (float(losses) / (roundCount * this->matchConfig.games)) * 100 << "% "
+       << "D:" << (float(draws) / (roundCount * this->matchConfig.games)) * 100 << "%\n";
     ss << "Elo difference: " << elo.getElo() << "\n---------------------------\n";
     std::cout << ss.str();
 }

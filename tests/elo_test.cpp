9d8f2f86b4454933ae5cf84b9564b7abc800b22d
disservin <45608332+Disservin@users.noreply.github.com>
2023-03-12 13:59:22 +0100

    namespaces

--- tests/elo_test.cpp
+++ tests/elo_test.cpp
@@ -6,7 +6,7 @@ using namespace fast_chess;
 
 TEST_CASE("Elo calculation")
 {
-    fast_chess::Elo elo(0, 36, 14);
+    Elo elo(0, 36, 14);
 
     CHECK(elo.getDiff(0, 36, 14) == doctest::Approx(-315.34).epsilon(0.01));
     CHECK(elo.getError(0, 36, 14) == doctest::Approx(95.43).epsilon(0.01));
@@ -14,7 +14,7 @@ TEST_CASE("Elo calculation")
 
 TEST_CASE("Elo calculation 2")
 {
-    fast_chess::Elo elo(859, 772, 1329);
+    Elo elo(859, 772, 1329);
 
     CHECK(elo.getDiff(859, 772, 1329) == doctest::Approx(10.21).epsilon(0.01));
     CHECK(elo.getError(859, 772, 1329) == doctest::Approx(9.28).epsilon(0.01));
@@ -22,7 +22,7 @@ TEST_CASE("Elo calculation 2")
 
 TEST_CASE("Elo calculation 3")
 {
-    fast_chess::Elo elo(1164, 1267, 3049);
+    Elo elo(1164, 1267, 3049);
 
     CHECK(elo.getDiff(1164, 1267, 3049) == doctest::Approx(-6.53).epsilon(0.01));
     CHECK(elo.getError(1164, 1267, 3049) == doctest::Approx(6.12).epsilon(0.01));

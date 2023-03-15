9c69c5a4d615d9ba44361276eb0ec9ee3e193a21
disservin <45608332+Disservin@users.noreply.github.com>
2023-03-15 23:59:20 +0100

    Update threadpool.hpp

--- src/threadpool.hpp
+++ src/threadpool.hpp
@@ -26,14 +26,14 @@ class ThreadPool
     {
         for (size_t i = 0; i < num_threads; ++i)
             workers_.emplace_back([this] {
-                for (;;)
+                while (true)
                 {
                     std::function<void()> task;
                     {
                         std::unique_lock<std::mutex> lock(this->queue_mutex_);
                         this->condition_.wait(
                             lock, [this] { return this->stop_ || !this->tasks_.empty(); });
-                        if (this->stop_ && this->tasks_.empty())
+                        if (this->stop_)
                             return;
                         task = std::move(this->tasks_.front());
                         this->tasks_.pop();

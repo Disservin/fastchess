9c2a59ab4e33a9ef604f80b22c4825c577893c16
Disservin <disservin.social@gmail.com>
2023-07-09 19:37:11 +0200

    fix: getline with different line endings

--- src/pgn_reader.cpp
+++ src/pgn_reader.cpp
@@ -7,6 +7,8 @@
 
 namespace fast_chess {
 
+std::istream& safeGetline(std::istream& is, std::string& t);
+
 PgnReader::PgnReader(const std::string& pgn_file_path) {
     // first we divide the file into single pgn objects
     // then we analyse each pgn object and extract the fen and moves
@@ -63,7 +65,7 @@ void PgnReader::analyseFile() {
     Opening pgn;
 
     std::string line;
-    while (std::getline(pgn_file_, line)) {
+    while (!safeGetline(pgn_file_, line).eof()) {
         if (line[0] == '[') {
             // we have a header
             in_header = true;
@@ -87,5 +89,36 @@ void PgnReader::analyseFile() {
         }
     }
     pgns_.push_back(pgn);
+}
+
+// https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
+std::istream& safeGetline(std::istream& is, std::string& t) {
+    t.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+
+    std::istream::sentry se(is, true);
+    std::streambuf* sb = is.rdbuf();
+
+    for (;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+            case '\n':
+                return is;
+            case '\r':
+                if (sb->sgetc() == '\n') sb->sbumpc();
+                return is;
+            case std::streambuf::traits_type::eof():
+                // Also handle the case when the last line has no line ending
+                if (t.empty()) is.setstate(std::ios::eofbit);
+                return is;
+            default:
+                t += (char)c;
+        }
+    }
 }
 }  // namespace fast_chess

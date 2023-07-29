3962a36032d3cbf6040ada2ba6c4eceaf35d52e5
disservin <disservin.social@gmail.com>
2023-07-29 14:43:00 +0200

    feat: better cutechess replacement support v6

--- src/cli.hpp
+++ src/cli.hpp
@@ -70,7 +70,7 @@ class OptionsParser {
         std::string month, day, year;
         std::stringstream ss, date(__DATE__);  // {month} {date} {year}
 
-        ss << "fast-chess ";
+        ss << "fast-chess alpha-1.0.0 ";
 #ifdef GIT_DATE
         ss << GIT_DATE;
 #else

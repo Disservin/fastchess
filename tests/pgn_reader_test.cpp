56da073374f2dfcdff5fb570ef2895fd2adb641c
disservin <disservin.social@gmail.com>
2023-08-14 13:13:27 +0200

    refactor: update chess lib and use lib's readGame functionality (#129)

--- tests/pgn_reader_test.cpp
+++ tests/pgn_reader_test.cpp
@@ -40,36 +40,83 @@ TEST_SUITE("PgnReader") {
             }
             return uci_moves;
         };
-        std::vector<std::string> moves;
-        std::vector<std::string> uci_moves;
-        std::vector<std::string> correct;
 
         // First game
-        moves = {"e4",   "e5",  "Nf3", "Nc6",  "Bc4",  "Nf6", "Ng5", "d5",
-                 "exd5", "Na5", "d3",  "Nxc4", "dxc4", "h6",  "Nf3", "e4"};
-        CHECK(games[0].moves == moves);
-        uci_moves = convert(moves);
-        correct = {"e2e4", "e7e5", "g1f3", "b8c6", "f1c4", "g8f6", "f3g5", "d7d5",
-                   "e4d5", "c6a5", "d2d3", "a5c4", "d3c4", "h7h6", "g5f3", "e5e4"};
-        CHECK(uci_moves == correct);
+        {
+            chess::Board board = chess::Board();
+
+            const std::vector<std::string> moves = {"e4",   "e5", "Nf3",  "Nc6", "Bc4", "Nf6",
+                                                    "Ng5",  "d5", "exd5", "Na5", "d3",  "Nxc4",
+                                                    "dxc4", "h6", "Nf3",  "e4"};
+
+            const auto uci_moves = convert(moves);
+
+            const std::vector<std::string> correct = {
+                "e2e4", "e7e5", "g1f3", "b8c6", "f1c4", "g8f6", "f3g5", "d7d5",
+                "e4d5", "c6a5", "d2d3", "a5c4", "d3c4", "h7h6", "g5f3", "e5e4"};
+
+            for (size_t i = 0; i < moves.size(); i++) {
+                const auto move = chess::uci::parseSan(board, moves[i]);
+                CHECK(games[0].moves[i] == move);
+                board.makeMove(move);
+            }
+
+            for (size_t i = 0; i < correct.size(); i++) {
+                CHECK(chess::uci::moveToUci(games[0].moves[i], false) == correct[i]);
+            }
+        }
 
         // Second game
-        moves = {"c4",   "c5", "Nf3", "Nc6", "d4",  "cxd4", "Nxd4", "Nxd4",
-                 "Qxd4", "d6", "Nc3", "e5",  "Qd3", "Be7",  "g3",   "h6"};
-        CHECK(games[1].moves == moves);
-        uci_moves = convert(moves);
-        correct = {"c2c4", "c7c5", "g1f3", "b8c6", "d2d4", "c5d4", "f3d4", "c6d4",
-                   "d1d4", "d7d6", "b1c3", "e7e5", "d4d3", "f8e7", "g2g3", "h7h6"};
-        CHECK(uci_moves == correct);
+        {
+            chess::Board board = chess::Board();
+
+            const std::vector<std::string> moves = {"c4",   "c5",   "Nf3",  "Nc6", "d4",  "cxd4",
+                                                    "Nxd4", "Nxd4", "Qxd4", "d6",  "Nc3", "e5",
+                                                    "Qd3",  "Be7",  "g3",   "h6"};
+
+            const auto uci_moves = convert(moves);
+
+            const std::vector<std::string> correct = {
+                "c2c4", "c7c5", "g1f3", "b8c6", "d2d4", "c5d4", "f3d4", "c6d4",
+                "d1d4", "d7d6", "b1c3", "e7e5", "d4d3", "f8e7", "g2g3", "h7h6"};
+
+            std::cout << "fen: " << board.getFen() << "\n";
+
+            for (size_t i = 0; i < moves.size(); i++) {
+                const auto move = chess::uci::parseSan(board, moves[i]);
+                CHECK(games[1].moves[i] == move);
+                board.makeMove(move);
+            }
+
+            for (size_t i = 0; i < correct.size(); i++) {
+                CHECK(chess::uci::moveToUci(games[1].moves[i], false) == correct[i]);
+            }
+        }
 
         // Third game
-        moves = {"e4", "e6",   "d4", "d5", "Nc3", "Nf6", "Bg5",  "Be7",
-                 "e5", "Nfd7", "h4", "f6", "Bd3", "c5",  "Qh5+", "Kf8"};
-        CHECK(games[2].moves == moves);
-        uci_moves = convert(moves);
-        correct = {"e2e4", "e7e6", "d2d4", "d7d5", "b1c3", "g8f6", "c1g5", "f8e7",
-                   "e4e5", "f6d7", "h2h4", "f7f6", "f1d3", "c7c5", "d1h5", "e8f8"};
-        CHECK(uci_moves == correct);
+        {
+            chess::Board board = chess::Board();
+
+            const std::vector<std::string> moves = {"e4",  "e6",  "d4",   "d5",   "Nc3", "Nf6",
+                                                    "Bg5", "Be7", "e5",   "Nfd7", "h4",  "f6",
+                                                    "Bd3", "c5",  "Qh5+", "Kf8"};
+
+            const auto uci_moves = convert(moves);
+
+            const std::vector<std::string> correct = {
+                "e2e4", "e7e6", "d2d4", "d7d5", "b1c3", "g8f6", "c1g5", "f8e7",
+                "e4e5", "f6d7", "h2h4", "f7f6", "f1d3", "c7c5", "d1h5", "e8f8"};
+
+            for (size_t i = 0; i < moves.size(); i++) {
+                const auto move = chess::uci::parseSan(board, moves[i]);
+                CHECK(games[2].moves[i] == move);
+                board.makeMove(move);
+            }
+
+            for (size_t i = 0; i < correct.size(); i++) {
+                CHECK(chess::uci::moveToUci(games[2].moves[i], false) == correct[i]);
+            }
+        }
     }
 }
 }  // namespace fast_chess

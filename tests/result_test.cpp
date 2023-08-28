43c8ea4fb6529729ee648fa1a8ac1db580838854
disservin <disservin.social@gmail.com>
2023-08-28 22:31:31 +0200

    Update result_test.cpp

--- tests/result_test.cpp
+++ tests/result_test.cpp
@@ -7,43 +7,76 @@
 #include "doctest/doctest.hpp"
 
 namespace fast_chess {
-TEST_SUITE("Result Tests") {
-    std::string engine1 = "engine1";
-    std::string engine2 = "engine2";
 
+TEST_SUITE("Result Tests") {
     TEST_CASE("Update and Get") {
-        Result result;
+        EngineConfiguration engine1 = {};
+        EngineConfiguration engine2 = {};
+
+        engine1.name = "engine1";
+        engine2.name = "engine2";
 
         Stats stats = Stats{.wins = 1, .losses = 2, .draws = 3};
-        result.updateStats(engine1, engine2, stats);
-        CHECK(result.getStats(engine1, engine2) == stats);
+
+        Result result;
+        result.updateStats({engine1, engine2}, stats);
+
+        CHECK(result.getStats(engine1.name, engine2.name) == stats);
     }
 
     TEST_CASE("Update and Update and Get") {
-        Result result;
+        EngineConfiguration engine1 = {};
+        EngineConfiguration engine2 = {};
+
+        engine1.name = "engine1";
+        engine2.name = "engine2";
 
         Stats stats = Stats{.wins = 1, .losses = 2, .draws = 3};
-        result.updateStats(engine1, engine2, stats);
-        result.updateStats(engine1, engine2, stats);
-        CHECK(result.getStats(engine1, engine2) == Stats{.wins = 2, .losses = 4, .draws = 6});
+
+        Result result;
+        result.updateStats({engine1, engine2}, stats);
+        result.updateStats({engine1, engine2}, stats);
+
+        CHECK(result.getStats(engine1.name, engine2.name) ==
+              Stats{.wins = 2, .losses = 4, .draws = 6});
     }
 
     TEST_CASE("Update and Update Reverse and Get") {
+        EngineConfiguration engine1 = {};
+        EngineConfiguration engine2 = {};
+
+        engine1.name = "engine1";
+        engine2.name = "engine2";
+
         Result result;
 
         Stats stats = Stats{.wins = 1, .losses = 2, .draws = 3};
-        result.updateStats(engine1, engine2, stats);
-        result.updateStats(engine2, engine1, stats);
-        CHECK(result.getStats(engine1, engine2) == Stats{.wins = 3, .losses = 3, .draws = 6});
-        CHECK(result.getStats(engine2, engine1) == Stats{.wins = 3, .losses = 3, .draws = 6});
+
+        result.updateStats({engine1, engine2}, stats);
+        result.updateStats({engine2, engine1}, stats);
+
+        CHECK(result.getStats(engine1.name, engine2.name) ==
+              Stats{.wins = 3, .losses = 3, .draws = 6});
+        CHECK(result.getStats(engine2.name, engine1.name) ==
+              Stats{.wins = 3, .losses = 3, .draws = 6});
     }
 
     TEST_CASE("SetResults") {
-        stats_map results = {{engine1, {{engine2, Stats{.wins = 1, .losses = 2, .draws = 3}}}},
-                             {engine2, {{engine1, Stats{.wins = 0, .losses = 0, .draws = 0}}}}};
+        EngineConfiguration engine1 = {};
+        EngineConfiguration engine2 = {};
+
+        engine1.name = "engine1";
+        engine2.name = "engine2";
+
+        stats_map results = {
+            {engine1.name, {{engine2.name, Stats{.wins = 1, .losses = 2, .draws = 3}}}},
+            {engine2.name, {{engine1.name, Stats{.wins = 0, .losses = 0, .draws = 0}}}}};
+
         Result result;
         result.setResults(results);
-        CHECK(result.getStats(engine1, engine2) == Stats{.wins = 1, .losses = 2, .draws = 3});
+
+        CHECK(result.getStats(engine1.name, engine2.name) ==
+              Stats{.wins = 1, .losses = 2, .draws = 3});
         CHECK(result.getResults() == results);
     }
 }

92ab10a6e5986c4796257efb4fb43714e5125e5e
disservin <disservin.social@gmail.com>
2023-09-03 15:54:41 +0200

    test: add random mover test (#135)

---
+++ tests/e2e/e2e.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+set -x
+
+# Compile the random_mover
+g++ -O3 -std=c++17 tests/e2e/random_mover.cpp -o random_mover
+
+# Compile fast-chess
+make -j
+
+OUTPUT_FILE=$(mktemp)
+./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
+    -each tc=2+0.02s -rounds 5 -repeat -concurrency 4 \
+    -openings file=tests/e2e/openings.txt format=epd order=random | tee $OUTPUT_FILE
+
+# Check if "Saved results." is in the output, else fail
+if ! grep -q "Saved results." $OUTPUT_FILE; then
+    echo "Failed to save results."
+    exit 1
+fi
+
+OUTPUT_FILE_2=$(mktemp)
+./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
+    -each tc=2+0.02s -rounds 5 -repeat -concurrency 4 \
+    -openings file=tests/e2e/openings.txt format=epd order=random | tee $OUTPUT_FILE_2
+
+# Check if "Saved results." is in the output, else fail
+if ! grep -q "Saved results." $OUTPUT_FILE_2; then
+    echo "Failed to save results."
+    exit 1
+fi
---
+++ tests/e2e/random_mover.cpp
@@ -0,0 +1,87 @@
+#include <random>
+
+#include "../../src/third_party/chess.hpp"
+
+using namespace chess;
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
+int random_number(int min, int max) {
+    std::uniform_int_distribution<> dis(min, max);
+    return dis(gen);
+}
+
+Move random_move(const Board &board) {
+    auto moves = Movelist();
+    movegen::legalmoves(moves, board);
+
+    return moves[random_number(0, moves.size() - 1)];
+}
+
+void uci_line(Board &board, const std::string &line) {
+    auto tokens = utils::splitString(line, ' ');
+
+    if (tokens.empty())
+        return;
+    else if (tokens[0] == "position") {
+        // parse position
+        if (tokens[1] == "startpos") {
+            board = Board();
+            if (tokens.size() > 2 && tokens[2] == "moves") {
+                for (int i = 3; i < tokens.size(); i++) {
+                    board.makeMove(uci::uciToMove(board, tokens[i]));
+                }
+            }
+        } else if (tokens[1] == "fen") {
+            std::string fen = "";
+            // parse until "moves"
+            int i = 2;
+            for (; i < tokens.size(); i++) {
+                if (tokens[i] == "moves") {
+                    i++;
+                    break;
+                };
+                fen += tokens[i] + " ";
+            }
+
+            board.setFen(fen);
+
+            for (; i < tokens.size(); i++) {
+                board.makeMove(uci::uciToMove(board, tokens[i]));
+            }
+        }
+    } else if (tokens[0] == "uci") {
+        std::cout << "id name random_move" << std::endl;
+        std::cout << "uciok" << std::endl;
+    } else if (tokens[0] == "isready") {
+        std::cout << "readyok" << std::endl;
+    } else if (tokens[0] == "ucinewgame") {
+        board = Board();
+    } else if (tokens[0] == "go") {
+        const auto move = random_move(board);
+        std::cout << "info depth 1 pv " << uci::moveToUci(move) << std::endl;
+        std::cout << "bestmove " << uci::moveToUci(move) << std::endl;
+    }
+}
+
+void uci_loop() {
+    std::string input;
+    std::cin >> std::ws;
+
+    Board board = Board();
+
+    while (true) {
+        if (!std::getline(std::cin, input) && std::cin.eof()) {
+            input = "quit";
+        }
+
+        if (input == "quit") {
+            return;
+        } else {
+            uci_line(board, input);
+        }
+    }
+}
+
+int main() { uci_loop(); }

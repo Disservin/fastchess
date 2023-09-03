517f98840a886645038a1ab6a1ba6ed4b42f9046
disservin <disservin.social@gmail.com>
2023-09-03 15:46:16 +0200

    Update e2e.sh

--- tests/e2e/e2e.sh
+++ tests/e2e/e2e.sh
@@ -10,8 +10,7 @@ make -j
 OUTPUT_FILE=$(mktemp)
 ./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 5 -repeat -concurrency 4 \
-    -openings file=tests/e2e/openings.txt format=epd order=random \
-    -draw movecount=8 score=8 movenumber=30 | tee $OUTPUT_FILE
+    -openings file=tests/e2e/openings.txt format=epd order=random | tee $OUTPUT_FILE
 
 # Check if "Saved results." is in the output, else fail
 if ! grep -q "Saved results." $OUTPUT_FILE; then
@@ -22,8 +21,7 @@ fi
 OUTPUT_FILE_2=$(mktemp)
 ./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 5 -repeat -concurrency 4 \
-    -openings file=tests/e2e/openings.txt format=epd order=random \
-    -draw movecount=8 score=8 movenumber=30 | tee $OUTPUT_FILE_2
+    -openings file=tests/e2e/openings.txt format=epd order=random | tee $OUTPUT_FILE_2
 
 # Check if "Saved results." is in the output, else fail
 if ! grep -q "Saved results." $OUTPUT_FILE_2; then

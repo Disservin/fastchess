129a4a7e37f792f28ee37bf855bb798c1d76717b
disservin <disservin.social@gmail.com>
2023-09-03 15:16:34 +0200

    random mover test

---
+++ tests/e2e/e2e.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+# Compile the random_mover
+make -j -O3 random_mover.cpp -o random_mover
+
+cd ../..
+
+OUTPUT_FILE=$(mktemp)
+./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
+    -each tc=2+0.02s -rounds 10 -repeat -concurrency 2 \
+    -openings file=tests/e2e/openings.txt format=epd order=random \
+    -draw movecount=8 score=8 movenumber=30 2>&1 | tee $OUTPUT_FILE
+
+# Check if "Saved results." is in the output, else fail
+if ! grep -q "Saved results." $OUTPUT_FILE; then
+    echo "Failed to save results."
+    exit 1
+fi
+
+OUTPUT_FILE_2=$(mktemp)
+./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
+    -each tc=2+0.02s -rounds 10 -repeat -concurrency 2 \
+    -openings file=tests/e2e/openings.txt format=epd order=random \
+    -draw movecount=8 score=8 movenumber=30 2>&1 | tee $OUTPUT_FILE_2
+
+# Check if "Saved results." is in the output, else fail
+if ! grep -q "Saved results." $OUTPUT_FILE_2; then
+    echo "Failed to save results."
+    exit 1
+fi

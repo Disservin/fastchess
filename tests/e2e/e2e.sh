e82a57ba2fd09d0386a1711e7b57d78f8a076336
disservin <disservin.social@gmail.com>
2023-09-10 22:01:27 +0200

    test: add pgn book test

--- tests/e2e/e2e.sh
+++ tests/e2e/e2e.sh
@@ -8,10 +8,12 @@ g++ -O3 -std=c++17 tests/e2e/random_mover.cpp -o random_mover
 # Compile fast-chess
 make -j
 
+# EPD Book Test
+
 OUTPUT_FILE=$(mktemp)
 ./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 5 -repeat -concurrency 4 \
-    -openings file=tests/e2e/openings.txt format=epd order=random | tee $OUTPUT_FILE
+    -openings file=tests/e2e/openings.epd format=epd order=random | tee $OUTPUT_FILE
 
 # Check if "Saved results." is in the output, else fail
 if ! grep -q "Saved results." $OUTPUT_FILE; then
@@ -19,10 +21,12 @@ if ! grep -q "Saved results." $OUTPUT_FILE; then
     exit 1
 fi
 
+# PGN Book Test
+
 OUTPUT_FILE_2=$(mktemp)
 ./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 5 -repeat -concurrency 4 \
-    -openings file=tests/e2e/openings.txt format=epd order=random | tee $OUTPUT_FILE_2
+    -openings file=tests/e2e/openings.pgn format=pgn order=random | tee $OUTPUT_FILE_2
 
 # Check if "Saved results." is in the output, else fail
 if ! grep -q "Saved results." $OUTPUT_FILE_2; then

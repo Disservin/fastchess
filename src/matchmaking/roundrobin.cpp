b0f4f114fdb7fc1b28427c5a108a7e457a49e0a7
disservin <disservin.social@gmail.com>
2023-10-29 14:29:04 +0100

    Update roundrobin.cpp

--- src/matchmaking/roundrobin.cpp
+++ src/matchmaking/roundrobin.cpp
@@ -22,7 +22,7 @@ RoundRobin::RoundRobin(const cmd::TournamentOptions& game_config)
     setupEpdOpeningBook();
     setupPgnOpeningBook();
 
-    // Resize the thread pool, but don't go over the number of cores the system has
+    // Resize the thread pool, but don't go over the number of threads the system has
     pool_.resize(std::min(static_cast<int>(std::thread::hardware_concurrency()),
                           tournament_options_.concurrency));
 

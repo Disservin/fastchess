d4ac030a1b78f64ede19319cdc74ec1e8a3bba96
Disservin <disservin.social@gmail.com>
2023-10-31 21:05:08 +0100

    fix script

--- tests/e2e/e2e.sh
+++ tests/e2e/e2e.sh
@@ -13,9 +13,9 @@ make -j $1
 OUTPUT_FILE=$(mktemp)
 ./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 5 -repeat -concurrency 2 \
-    -openings file=tests/e2e/openings.epd format=epd order=random | tee $OUTPUT_FILE
+    -openings file=tests/e2e/openings.epd format=epd order=random 2>&1 | tee $OUTPUT_FILE
 
-if grep -q "ThreadSanitizer:" $OUTPUT_FILE; then
+if grep -q "WARNING: ThreadSanitizer:" $OUTPUT_FILE; then
     echo "Data races detected."
     exit 1
 fi
@@ -31,9 +31,11 @@ fi
 OUTPUT_FILE_2=$(mktemp)
 ./fast-chess -engine cmd=random_mover name=random_move_1 -engine cmd=random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 5 -repeat -concurrency 2 \
-    -openings file=tests/e2e/openings.pgn format=pgn order=random | tee $OUTPUT_FILE_2
+    -openings file=tests/e2e/openings.pgn format=pgn order=random 2>&1 | tee $OUTPUT_FILE_2
 
-if grep -q "ThreadSanitizer:" $OUTPUT_FILE; then
+less $OUTPUT_FILE_2
+
+if grep -q "WARNING: ThreadSanitizer:" $OUTPUT_FILE_2; then
     echo "Data races detected."
     exit 1
 fi

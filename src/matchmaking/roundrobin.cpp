ca0c3c79e8706611355ba0f8bc8fc911917b0424
disservin <disservin.social@gmail.com>
2023-11-18 23:41:22 +0100

    refactor: move limit into threadpool

--- src/matchmaking/roundrobin.cpp
+++ src/matchmaking/roundrobin.cpp
@@ -25,9 +25,7 @@ RoundRobin::RoundRobin(const cmd::TournamentOptions& game_config)
     setupEpdOpeningBook();
     setupPgnOpeningBook();
 
-    // Resize the thread pool, but don't go over the number of threads the system has
-    pool_.resize(std::min(static_cast<int>(std::thread::hardware_concurrency()),
-                          tournament_options_.concurrency));
+    pool_.resize(tournament_options_.concurrency);
 
     // Initialize the SPRT test
     sprt_ = SPRT(tournament_options_.sprt.alpha, tournament_options_.sprt.beta,

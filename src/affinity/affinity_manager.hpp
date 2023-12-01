f2ae388def4a5275bf264da9f7a7a57b0a8b1fd3
disservin <disservin.social@gmail.com>
2023-12-01 13:29:08 +0100

    Update affinity_manager.hpp

--- src/affinity/affinity_manager.hpp
+++ src/affinity/affinity_manager.hpp
@@ -59,32 +59,6 @@ class AffinityManager {
         }
     }
 
-    /// @brief Setup the cores for the affinity, later entries from the core pool will be just
-    /// picked up.
-    void setupCores() {
-        std::lock_guard<std::mutex> lock(core_mutex_);
-
-        // physical_id is the id for the physical cpu, will be 0 for single cpu systems
-        // cores is array of two vectors, each vector contains distinct processors which are
-        // not on the same physical core. We distribute the workload first over all cores
-        // in HT_1, then over all cores in HT_2.
-
-        /// @todo: fix logic for multiple threads and multiple concurrencies
-
-        for (const auto& physical_cpu : cpu_info.physical_cpus) {
-            for (const auto& core : physical_cpu.second.cores) {
-                int idx = 0;
-                for (const auto& processor : core.second.processors) {
-                    Group group = idx % 2 == 0 ? HT_1 : HT_2;
-
-                    cores_[group].emplace_back(std::vector{processor.processor_id});
-
-                    idx++;
-                }
-            }
-        }
-    }
-
     /// @brief Get a core from the pool of available cores.
     ///
     /// @return
@@ -113,6 +87,32 @@ class AffinityManager {
     }
 
    private:
+    /// @brief Setup the cores for the affinity, later entries from the core pool will be just
+    /// picked up.
+    void setupCores() {
+        std::lock_guard<std::mutex> lock(core_mutex_);
+
+        // physical_id is the id for the physical cpu, will be 0 for single cpu systems
+        // cores is array of two vectors, each vector contains distinct processors which are
+        // not on the same physical core. We distribute the workload first over all cores
+        // in HT_1, then over all cores in HT_2.
+
+        /// @todo: fix logic for multiple threads and multiple concurrencies
+
+        for (const auto& physical_cpu : cpu_info.physical_cpus) {
+            for (const auto& core : physical_cpu.second.cores) {
+                int idx = 0;
+                for (const auto& processor : core.second.processors) {
+                    Group group = idx % 2 == 0 ? HT_1 : HT_2;
+
+                    cores_[group].emplace_back(std::vector{processor.processor_id});
+
+                    idx++;
+                }
+            }
+        }
+    }
+
     CpuInfo cpu_info;
     std::array<std::deque<AffinityProcessor>, 2> cores_;
     std::mutex core_mutex_;

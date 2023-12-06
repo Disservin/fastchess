ec79854a27f5ac7800b3204c3391884dc81b9754
disservin <disservin.social@gmail.com>
2023-12-06 15:38:05 +0100

    Update globals.cpp

--- src/globals/globals.cpp
+++ src/globals/globals.cpp
@@ -16,8 +16,7 @@ std::atomic_bool stop = false;
 ThreadVector<HANDLE> process_list;
 #else
 #include <unistd.h>
-#include <signal.h>
-
+#include <csignal>
 ThreadVector<pid_t> process_list;
 #endif
 

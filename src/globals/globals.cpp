f551fcecd86766a238e749b9d16c7a949ff8f5d2
disservin <disservin.social@gmail.com>
2023-12-06 16:17:19 +0100

    refactor: cleanup main.cpp (#184)

---
+++ src/globals/globals.cpp
@@ -0,0 +1,71 @@
+#include <globals/globals.hpp>
+
+#include <util/thread_vector.hpp>
+#include <util/logger.hpp>
+
+namespace fast_chess {
+
+#include <atomic>
+
+namespace atomic {
+std::atomic_bool stop = false;
+}
+
+#ifdef _WIN64
+#include <windows.h>
+ThreadVector<HANDLE> process_list;
+#else
+#include <unistd.h>
+#include <signal.h>
+#include <cstdlib>
+ThreadVector<pid_t> process_list;
+#endif
+
+/// @brief Make sure that all processes are stopped, and no zombie processes are left after the
+/// program exits.
+void stopProcesses() {
+#ifdef _WIN64
+    for (const auto &pid : process_list) {
+        TerminateProcess(pid, 1);
+        CloseHandle(pid);
+    }
+#else
+    for (const auto &pid : process_list) {
+        kill(pid, SIGINT);
+        kill(pid, SIGKILL);
+    }
+#endif
+}
+
+void consoleHandlerAction() { fast_chess::atomic::stop = true; }
+
+#ifdef _WIN64
+BOOL WINAPI handler(DWORD signal) {
+    switch (signal) {
+        case CTRL_CLOSE_EVENT:
+        case CTRL_LOGOFF_EVENT:
+        case CTRL_SHUTDOWN_EVENT:
+        case CTRL_C_EVENT:
+            consoleHandlerAction();
+        default:
+            break;
+    }
+    return TRUE;
+}
+
+void setCtrlCHandler() {
+    if (!SetConsoleCtrlHandler(handler, TRUE)) {
+        Logger::log<Logger::Level::FATAL>("Could not set control handler.");
+    }
+}
+
+#else
+void handler(int param) {
+    consoleHandlerAction();
+    std::exit(param);
+}
+
+void setCtrlCHandler() { signal(SIGINT, handler); }
+#endif
+
+}  // namespace fast_chess
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,25 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.hpp"
 
-#include <util/thread_vector.hpp>
-
-namespace fast_chess {
-namespace atomic {
-std::atomic_bool stop = false;
-}  // namespace atomic
-}  // namespace fast_chess
-
-namespace fast_chess {
-
-#ifdef _WIN64
-#include <windows.h>
-ThreadVector<HANDLE> process_list;
-#else
-#include <unistd.h>
-ThreadVector<pid_t> process_list;
-#endif
-}  // namespace fast_chess
-
 int main(int argc, char **argv) {
     doctest::Context ctx;
 

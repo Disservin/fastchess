04b13f54e59d5f299d7685b32ac3b6304f7e30c0
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-12 04:51:46 +0700

    Update elo.cpp

--- src/matchmaking/elo/elo.cpp
+++ src/matchmaking/elo/elo.cpp
@@ -47,8 +47,8 @@ double Elo::getError(int wins, int losses, int draws) noexcept {
     const double devD  = d * std::pow(0.5 - perc, 2.0);
     const double stdev = std::sqrt(devW + devL + devD) / std::sqrt(n);
 
-    const double devMin = perc + phiInv(0.025) * stdev;
-    const double devMax = perc + phiInv(0.975) * stdev;
+    const double devMin = a + phiInv(0.025) * stdev;
+    const double devMax = a + phiInv(0.975) * stdev;
     return (percToEloDiff(devMax) - percToEloDiff(devMin)) / 2.0;
 }
 
@@ -88,7 +88,6 @@ double Elo::getDiff(int penta_WW, int penta_WD, int penta_WL, int penta_DD, int 
     const double WL = double(penta_WL) / pairs; 
     const double DD = double(penta_DD) / pairs;
     const double LD = double(penta_LD) / pairs;
-    const double LL = double(penta_LL) / pairs;
     const double percentage = WW + 0.75 * WD + 0.5 * (WL + DD) + 0.25 * LD;
 
     return percToEloDiff(percentage);

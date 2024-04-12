1abb64ce5e2d2faf69ff7f268a63c4a9fc3f79a5
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-12 12:32:41 +0700

    Update sprt.hpp

--- src/matchmaking/sprt/sprt.hpp
+++ src/matchmaking/sprt/sprt.hpp
@@ -14,7 +14,8 @@ class SPRT {
 
     [[nodiscard]] bool isValid() const noexcept;
 
-    [[nodiscard]] static double neloToScore(double nelo, double stdDeviation) noexcept;
+    [[nodiscard]] static double neloToScoreWDL(double nelo, double stdDeviation) noexcept;
+    [[nodiscard]] static double neloToScorePenta(double nelo, double stdDeviation) noexcept;
     [[nodiscard]] double getLLR(int win, int draw, int loss) const noexcept;
     [[nodiscard]] double getLLR(int penta_WW, int penta_WD, int penta_WL, int penta_DD, int penta_LD, int penta_LL) const noexcept;
 

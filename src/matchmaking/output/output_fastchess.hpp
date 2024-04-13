c59f222508a3189011fbea3b4091f467e1abb112
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-13 21:59:27 +0700

    Update output_fastchess.hpp

--- src/matchmaking/output/output_fastchess.hpp
+++ src/matchmaking/output/output_fastchess.hpp
@@ -15,12 +15,13 @@ class Fastchess : public IOutput {
         std::cout << "--------------------------------------------------\n";
         printElo(stats, first, second, current_game_count);
         printSprt(sprt, stats);
-        printPenta(stats);
+        if (report_penta_ == true){printPenta(stats);}
         std::cout << "--------------------------------------------------\n";
     };
 
     void printElo(const Stats& stats, const std::string& first, const std::string& second,
                   std::size_t current_game_count) override {
+        const Elo elo(stats.wins, stats.losses, stats.draws);
         const Elo elo(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
                       stats.penta_LD, stats.penta_LL);
 
@@ -36,27 +37,40 @@ class Fastchess : public IOutput {
            << "L - "        //
            << stats.draws   //
            << "D ["         //
-           << Elo::getScoreRatio(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
-                                 stats.penta_LD, stats.penta_LL)  //
+           << Elo::getScoreRatio(stats.wins, stats.losses, stats.draws)  //
            << "] "                                                //
            << current_game_count                                  //
            << "\n";
-
-        ss << "Elo difference: "   //
-           << elo.getElo()         //
-           << ", "                 //
-           << "nElo difference: "  //
-           << elo.getnElo()        //
-           << ", "                 //
-           << "LOS: "              //
-           << Elo::getLos(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
-                          stats.penta_LD, stats.penta_LL)  //
-           << ", "                                         //
-           << "PairDrawRatio: "                            //
-           << Elo::getDrawRatio(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
-                                stats.penta_LD, stats.penta_LL)  //
-           << "\n";
-
+       
+        if (report_penta_ == true){
+           ss << "Elo difference: "   //
+              << elo.getEloPenta()         //
+              << ", "                 //
+              << "nElo difference: "  //
+              << elo.getnEloPenta()        //
+              << ", "                 //
+              << "LOS: "              //
+              << Elo::getLos(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
+                             stats.penta_LD, stats.penta_LL)  //
+              << ", "                                         //
+              << "PairDrawRatio: "                            //
+              << Elo::getDrawRatio(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
+                                   stats.penta_LD, stats.penta_LL)  //
+              << "\n";
+        } else {
+           ss << "Elo difference: "   //
+              << elo.getElo()         //
+              << ", "                 //
+              << "nElo difference: "  //
+              << elo.getnElo()        //
+              << ", "                 //
+              << "LOS: "              //
+              << Elo::getLos(stats.wins, stats.losses, stats.draws)  //
+              << ", "                                         //
+              << "PairDrawRatio: "                            //
+              << Elo::getDrawRatio(stats.wins, stats.losses, stats.draws)  //
+              << "\n";
+        }
         std::cout << ss.str() << std::flush;
     }
 
@@ -64,10 +78,16 @@ class Fastchess : public IOutput {
         if (sprt.isValid()) {
             std::stringstream ss;
 
-            ss << "LLR: " << std::fixed << std::setprecision(2)
-               << sprt.getLLR(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
-                              stats.penta_LD, stats.penta_LL)
-               << " " << sprt.getBounds() << " " << sprt.getElo() << "\n";
+            if (report_penta_ == true){
+               ss << "LLR: " << std::fixed << std::setprecision(2)
+                  << sprt.getLLR(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD,
+                                 stats.penta_LD, stats.penta_LL)
+                  << " " << sprt.getBounds() << " " << sprt.getElo() << "\n";
+            } else {
+               ss << "LLR: " << std::fixed << std::setprecision(2)
+                  << sprt.getLLR(stats.wins, stats.draws, stats.losses)
+                  << " " << sprt.getBounds() << " " << sprt.getElo() << "\n";
+            }
             std::cout << ss.str() << std::flush;
         }
     };

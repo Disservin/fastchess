efd6f58a899a5270a6792c80abbfa8549e3b85a0
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-14 12:58:39 +0700

    Update output_fastchess.hpp

--- src/matchmaking/output/output_fastchess.hpp
+++ src/matchmaking/output/output_fastchess.hpp
@@ -47,9 +47,10 @@ class Fastchess : public IOutput {
            << " DrawRatio: "          //
            << elo->drawRatio(stats);  //
 
+        const double pairsRatio = static_cast<double>(stats.penta_WW + stats.penta_WD) / (stats.penta_LD + stats.penta_LL)
         if (report_penta_){
            ss << "PairsRatio: "
-              << elo->pairsRatio
+              << pairsRatio
               << "\n";
         } else {
            ss << "\n";

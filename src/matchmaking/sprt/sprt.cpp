1fc04448d7ea695c197b46602454ad38bc2ab174
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-14 07:39:55 +0700

    Update sprt.cpp

--- src/matchmaking/sprt/sprt.cpp
+++ src/matchmaking/sprt/sprt.cpp
@@ -8,7 +8,7 @@
 
 namespace fast_chess {
 
-SPRT::SPRT(double alpha, double beta, double elo0, double elo1, bool model) {
+SPRT::SPRT(double alpha, double beta, double elo0, double elo1, bool logisticBounds) {
     valid_ = alpha != 0.0 && beta != 0.0 && elo0 < elo1;
     if (isValid()) {
         lower_ = std::log(beta / (1 - alpha));
@@ -17,7 +17,7 @@ SPRT::SPRT(double alpha, double beta, double elo0, double elo1, bool model) {
         elo0_ = elo0;
         elo1_ = elo1;
 
-        logisticBounds_ = model;
+        logisticBounds_ = logisticBounds;
 
         Logger::log<Logger::Level::INFO>("Initialized valid SPRT configuration.");
     } else if (!(alpha == 0.0 && beta == 0.0 && elo0 == 0.0 && elo1 == 0.0)) {

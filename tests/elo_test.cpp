c9babf3581b1261b132afb1471a60d467423361c
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-14 07:22:31 +0700

    Update elo_test.cpp

--- tests/elo_test.cpp
+++ tests/elo_test.cpp
@@ -1,4 +1,4 @@
-#include <matchmaking/elo/elo_logistic.hpp>
+#include <matchmaking/elo/elo_wdl.hpp>
 
 #include "doctest/doctest.hpp"
 
@@ -9,7 +9,7 @@ using namespace fast_chess;
 TEST_SUITE("Elo Calculation Tests") {
     TEST_CASE("Elo calculation") {
         Stats stats(0, 36, 14);
-        EloLogistic elo(stats);
+        EloWDL elo(stats);
 
         // CHECK(elo.diff(stats) == doctest::Approx(-315.34).epsilon(0.01));
         // CHECK(elo.error(stats) == doctest::Approx(95.43).epsilon(0.01));
@@ -17,7 +17,7 @@ TEST_SUITE("Elo Calculation Tests") {
 
     TEST_CASE("Elo calculation 2") {
         Stats stats(859, 772, 1329);
-        EloLogistic elo(stats);
+        EloWDL elo(stats);
 
         // CHECK(elo.diff(stats) == doctest::Approx(10.21).epsilon(0.01));
         // CHECK(elo.error(stats) == doctest::Approx(9.28).epsilon(0.01));
@@ -25,7 +25,7 @@ TEST_SUITE("Elo Calculation Tests") {
 
     TEST_CASE("Elo calculation 3") {
         Stats stats(1164, 1267, 3049);
-        EloLogistic elo(stats);
+        EloWDL elo(stats);
 
         // CHECK(elo.diff(stats) == doctest::Approx(-6.53).epsilon(0.01));
         // CHECK(elo.error(stats) == doctest::Approx(6.12).epsilon(0.01));

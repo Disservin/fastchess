5a2a9557d4ab87f49b78780ff80bde8d897ca214
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-15 16:44:50 +0700

    Update elo_wdl.hpp

--- src/matchmaking/elo/elo_wdl.hpp
+++ src/matchmaking/elo/elo_wdl.hpp
@@ -14,7 +14,7 @@ class EloWDL : public EloBase {
     [[nodiscard]] std::string los(const Stats& stats) const noexcept override;
     [[nodiscard]] std::string drawRatio(const Stats& stats) const noexcept override;
     [[nodiscard]] std::string scoreRatio(const Stats& stats) const noexcept override;
-    [[nodiscard]] std::string nElo() const noexcept;
+    [[nodiscard]] std::string nElo() const noexcept override;
 
    private:
     [[nodiscard]] static double percToEloDiff(double percentage) noexcept;

4d1a531cfdfa7287d353c37702c66f8a2b6e4d47
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-27 18:29:44 +0700

    make it so seed randomizer cant output default seed

--- src/matchmaking/tournament/tournament_manager.cpp
+++ src/matchmaking/tournament/tournament_manager.cpp
@@ -10,10 +10,12 @@ TournamentManager::TournamentManager(const options::Tournament& tournament_confi
     validateEngines();
 
     if (tournament_options_.randomseed == true && tournament_options_.seed == 951356066) {
-        std::random_device rd;   // Random device to seed the generator
-        std::mt19937 gen(rd());  // Mersenne Twister 19937 generator
-        std::uniform_int_distribution<uint32_t> dist(0, std::numeric_limits<uint32_t>::max());
-        tournament_options_.seed = dist(gen);
+       while(tournament_options_.seed == 951356066){
+          std::random_device rd;   // Random device to seed the generator
+          std::mt19937 gen(rd());  // Mersenne Twister 19937 generator
+          std::uniform_int_distribution<uint32_t> dist(0, std::numeric_limits<uint32_t>::max());
+          tournament_options_.seed = dist(gen);
+       }
     }
 
     // Set the seed for the random number generator

2ac30aa2ece3868be9a3575075df59de29f1dbdd
Disservin <disservin.social@gmail.com>
2024-04-28 17:32:30 +0200

    fix

--- src/cli/cli.hpp
+++ src/cli/cli.hpp
@@ -71,7 +71,7 @@ class OptionsParser {
 #else
         date >> month >> day >> year;
         if (day.length() == 1) day = "0" + day;
-        ss << year.substr(2) << months[month] << day;
+        ss << year.substr(2) << months.at(month) << day;
 #endif
 
 #ifdef GIT_SHA

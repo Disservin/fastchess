b7c0a208249f2ad2108769d0425c08a4d4022161
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-04-30 06:51:24 +0700

    Update cli.cpp

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -115,7 +115,7 @@ void parseEngineKeyValues(EngineConfiguration &engineConfig, const std::string &
         const std::string strippedKey = key.substr(pos + 1);
         engineConfig.options.emplace_back(strippedKey, value);
     } else if (key == "proto") {
-        if (value != uci) {
+        if (value != "uci") {
             throw std::runtime_error("Error; unknown protocol");
         }
     } else

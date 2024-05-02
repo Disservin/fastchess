4ca10a3d73ca7cbe9888d35b3a43e6fbc8e764e0
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-03 00:23:51 +0700

    refactor: send go infinite instead of go (#340)

--- src/matchmaking/player.hpp
+++ src/matchmaking/player.hpp
@@ -75,6 +75,10 @@ class Player {
                 input << " movestogo " << time_control_.getMovesLeft();
             }
         }
+       
+        if (engine.getConfig().limit.plies == 0 && engine.getConfig().limit.nodes == 0
+           && !time_control_.isTimed() && !time_control_.isFixedTime())
+            input << " infinite";
 
         return input.str();
     }

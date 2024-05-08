4d1c6c8bc96f73fb4f7c2747b9b3504dcbef31b4
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-08 15:07:40 +0700

    Update tournament_options.hpp

--- src/types/tournament_options.hpp
+++ src/types/tournament_options.hpp
@@ -65,13 +65,6 @@ struct MaxMovesAdjudication {
 };
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(MaxMovesAdjudication, move_count, enabled)
 
-struct AutoSave {
-    int interval = 20;
-
-    bool enabled = true;
-};
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(AutoSave, interval, enabled)
-
 struct Tournament {
     Opening opening = {};
     Pgn pgn         = {};
@@ -99,7 +92,8 @@ struct Tournament {
 
     uint32_t seed = 951356066;
 
-    int ratinginterval = 10;
+    int ratinginterval   = 10;
+    int autosaveinterval = 20;
 
     int games       = 2;
     int rounds      = 2;
@@ -111,9 +105,9 @@ struct Tournament {
     bool affinity     = false;
     bool randomseed   = false;
 };
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(Tournament, resign, draw, maxmoves, autosave, opening, pgn, epd, 
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(Tournament, resign, draw, maxmoves, opening, pgn, epd, 
                                                 sprt, event_name, site, output, seed, variant,
-                                                ratinginterval, games, rounds, concurrency,
+                                                ratinginterval, autosaveinterval, games, rounds, concurrency,
                                                 overhead, recover, report_penta, affinity, randomseed)
 
 }  // namespace fast_chess::options

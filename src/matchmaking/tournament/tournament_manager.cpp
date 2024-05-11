637d6d2186e726269e9eff2b5ab40497fb642a14
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-11 09:00:12 +0700

    Update tournament_manager.cpp

--- src/matchmaking/tournament/tournament_manager.cpp
+++ src/matchmaking/tournament/tournament_manager.cpp
@@ -67,6 +67,12 @@ options::Tournament TournamentManager::fixConfig(options::Tournament config) {
     if (config.ratinginterval == 0) config.ratinginterval = std::numeric_limits<int>::max();
 
     if (config.scoreinterval == 0) config.scoreinterval = std::numeric_limits<int>::max();
+
+    if (config.seed == 951356066 && !config.randomseed) {
+        Logger::log<Logger::Level::WARN>(
+            "Warning: No opening book seed specified! Consider using one, otherwise the match will be "
+            "played using the default seed of 951356066.");
+    }
   
     return config;
 }

e6fec108f795d1f6b84df74fe131774ec0f6ad32
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-11 08:08:09 +0700

    Update cli.cpp

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -438,6 +438,10 @@ void parseRatinginterval(int &i, int argc, char const *argv[], ArgumentData &arg
     parseValue(i, argc, argv, argument_data.tournament_options.ratinginterval);
 }
 
+void parseScoreinterval(int &i, int argc, char const *argv[], ArgumentData &argument_data) {
+    parseValue(i, argc, argv, argument_data.tournament_options.scoreinterval);
+}
+
 void parseSRand(int &i, int argc, char const *argv[], ArgumentData &argument_data) {
     parseValue(i, argc, argv, argument_data.tournament_options.seed);
 }
@@ -561,6 +565,7 @@ OptionsParser::OptionsParser(int argc, char const *argv[]) {
     addOption("games", parseGames);
     addOption("rounds", parseRounds);
     addOption("ratinginterval", parseRatinginterval);
+    addOption("scoreinterval", parseScoreinterval);
     addOption("srand", parseSRand);
     addOption("version", parseVersion);
     addOption("-version", parseVersion);

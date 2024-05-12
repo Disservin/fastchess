5c9d8aacb6c72b26b33a245a2fe0f4461c546d87
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-12 18:21:04 +0700

    Update tournament_options.hpp

--- src/types/tournament_options.hpp
+++ src/types/tournament_options.hpp
@@ -53,10 +53,11 @@ NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(DrawAdjudication, move_number, m
 struct ResignAdjudication {
     int move_count = 1;
     int score      = 0;
+    bool twosided  = false;
 
     bool enabled = false;
 };
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(ResignAdjudication, move_count, score, enabled)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(ResignAdjudication, move_count, score, twosided, enabled)
 
 struct MaxMovesAdjudication {
     int move_count = 1;

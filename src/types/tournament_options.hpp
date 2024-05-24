ce650d2577d49445d1c8141d5f45cc2eb623d9cd
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-25 03:56:17 +0700

    Update tournament_options.hpp

--- src/types/tournament_options.hpp
+++ src/types/tournament_options.hpp
@@ -39,9 +39,9 @@ struct Sprt {
     double beta          = 0.0;
     double elo0          = 0.0;
     double elo1          = 0.0;
-    bool logistic_bounds = false;
+    std::string model    = "normalized";
 };
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(Sprt, alpha, beta, elo0, elo1, logistic_bounds)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(Sprt, alpha, beta, elo0, elo1, model)
 
 struct DrawAdjudication {
     int move_number = 0;

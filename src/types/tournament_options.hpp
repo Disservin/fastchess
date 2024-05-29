5f80945fd1e55387b47431066fd0f7c04a9adfaf
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-30 02:33:46 +0700

    Update tournament_options.hpp

--- src/types/tournament_options.hpp
+++ src/types/tournament_options.hpp
@@ -39,6 +39,8 @@ struct Sprt {
     double beta          = 0.0;
     double elo0          = 0.0;
     double elo1          = 0.0;
+    // available as 3 models: "normalized", "bayesian", and "logistic"
+    // bayesian model only available when -penta report=false
     std::string model    = "normalized";
 };
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ORDERED_JSON(Sprt, alpha, beta, elo0, elo1, model)

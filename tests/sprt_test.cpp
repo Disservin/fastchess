e63c0feb671e9bf97f447ffd9799b43905567b2d
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-05-31 03:50:08 +0700

    Update sprt_test.cpp

--- tests/sprt_test.cpp
+++ tests/sprt_test.cpp
@@ -1,5 +1,4 @@
-#include <elo/elo_pentanomial.hpp>
-#include <elo/elo_wdl.hpp>
+#include <matchmaking/sprt/sprt.hpp>
 
 #include "doctest/doctest.hpp"
 
@@ -7,61 +6,74 @@
 
 using namespace fast_chess;
 
-TEST_SUITE("Elo Model") {
-    TEST_CASE("EloWDL 1") {
-        Stats stats(76, 89, 123);
-        elo::EloWDL elo(stats);
+TEST_SUITE("SPRT") {
+    TEST_CASE("normalized trinomial 1") {
+        Stats stats(36433, 36027, 68692);
+        SPRT sprt(0.05, 0.05, 0, 2, "normalized");
 
-        CHECK(elo.nEloDiff() == doctest::Approx(-20.76).epsilon(0.01));
-        CHECK(elo.nEloError() == doctest::Approx(40.13).epsilon(0.01));
+        CHECK(sprt.getllr(stats, false) == doctest::Approx(0.92).epsilon(0.01));
     }
 
-    TEST_CASE("EloWDL 2") {
-        Stats stats(136, 96, 111);
-        elo::EloWDL elo(stats);
+    TEST_CASE("normalized trinomial 2") {
+        Stats stats(10871, 10650, 20431);
+        SPRT sprt(0.05, 0.05, -1.75, 0.25, "normalized");
 
-        CHECK(elo.nEloDiff() == doctest::Approx(49.77).epsilon(0.01));
-        CHECK(elo.nEloError() == doctest::Approx(36.77).epsilon(0.01));
+        CHECK(sprt.getllr(stats, false) == doctest::Approx(2.30).epsilon(0.01));
     }
 
-    TEST_CASE("EloPentanomial 3") {
-        Stats stats(34, 356, 0);
-        elo::EloWDL elo(stats);
+    TEST_CASE("logistic trinomial 1") {
+        Stats stats(21404, 21184, 40708);
+        SPRT sprt(0.05, 0.05, 0.5, 2.5, "logistic");
 
-        CHECK(elo.nEloDiff() == doctest::Approx(-508.44).epsilon(0.01));
-        CHECK(elo.nEloError() == doctest::Approx(34.48).epsilon(0.01));
+        CHECK(sprt.getllr(stats, false) == doctest::Approx(-1.57).epsilon(0.01));
     }
 
-    TEST_CASE("EloPentanomial 1") {
-        Stats stats(34, 54, 31, 32, 64, 75);
-        elo::EloPentanomial elo(stats);
+    TEST_CASE("logistic trinomial 2") {
+        Stats stats(57433, 57030, 106593);
+        SPRT sprt(0.05, 0.05, 0, 2, "logistic");
 
-        CHECK(elo.nEloDiff() == doctest::Approx(57.94).epsilon(0.01));
-        CHECK(elo.nEloError() == doctest::Approx(28.28).epsilon(0.01));
-
-        CHECK(elo.diff() == doctest::Approx(55.58).epsilon(0.01));
-        CHECK(elo.error() == doctest::Approx(27.65).epsilon(0.01));
+        CHECK(sprt.getllr(stats, false) == doctest::Approx(-2.59).epsilon(0.01));
     }
 
-    TEST_CASE("EloPentanomial 2") {
-        Stats stats(332, 433, 457, 41, 333, 334);
-        elo::EloPentanomial elo(stats);
+    TEST_CASE("bayesian trinomial 1") {
+        Stats stats(68965, 68526, 128429);
+        SPRT sprt(0.05, 0.05, 0, 2, "bayesian");
 
-        CHECK(elo.nEloDiff() == doctest::Approx(-9.17).epsilon(0.01));
-        CHECK(elo.nEloError() == doctest::Approx(10.96).epsilon(0.01));
-
-        CHECK(elo.diff() == doctest::Approx(-8.64).epsilon(0.01));
-        CHECK(elo.error() == doctest::Approx(10.33).epsilon(0.01));
+        CHECK(sprt.getllr(stats, false) == doctest::Approx(-1.26).epsilon(0.01));
     }
 
-    TEST_CASE("EloPentanomial 3") {
-        Stats stats(7895, 8757, 5485, 200, 568, 9999);
-        elo::EloPentanomial elo(stats);
+    TEST_CASE("bayesian trinomial 2") {
+        Stats stats(21629, 21484, 41111);
+        SPRT sprt(0.05, 0.05, 0.5, 2.5, "bayesian");
 
-        CHECK(elo.nEloDiff() == doctest::Approx(-19.01).epsilon(0.01));
-        CHECK(elo.nEloError() == doctest::Approx(2.65).epsilon(0.01));
+        CHECK(sprt.getllr(stats, false) == doctest::Approx(-1.13).epsilon(0.01));
+    }
 
-        CHECK(elo.diff() == doctest::Approx(-21.04).epsilon(0.01));
-        CHECK(elo.error() == doctest::Approx(2.95).epsilon(0.01));
+    TEST_CASE("normalized pentanomial 1") {
+        Stats stats(365, 16618, 36029, 200, 16974, 390);
+        SPRT sprt(0.05, 0.05, 0, 2, "normalized");
+
+        CHECK(sprt.getllr(stats, true) == doctest::Approx(2.25).epsilon(0.01));
+    }
+
+    TEST_CASE("normalized pentanomial 2") {
+        Stats stats(127, 4883, 10311, 401, 5150, 104);
+        SPRT sprt(0.05, 0.05, -1.75, 0.25, "normalized");
+
+        CHECK(sprt.getllr(stats, true) == doctest::Approx(3.01).epsilon(0.01));
+    }
+
+    TEST_CASE("logistic pentanomial 1") {
+        Stats stats(223, 9863, 20279, 1000, 10037, 246);
+        SPRT sprt(0.05, 0.05, 0.5, 2.5, "logistic");
+
+        CHECK(sprt.getllr(stats, true) == doctest::Approx(-3.07).epsilon(0.01));
+    }
+
+    TEST_CASE("logistic pentanomial 2") {
+        Stats stats(871, 26175, 55003, 980, 26678, 821);
+        SPRT sprt(0.05, 0.05, 0, 2, "logistic");
+
+        CHECK(sprt.getllr(stats, true) == doctest::Approx(-4.98).epsilon(0.01));
     }
 }

05da694c6843f8006e0e4f26d1ce91118272e854
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-04 04:56:09 +0700

    Update roundrobin.cpp

--- src/matchmaking/tournament/roundrobin/roundrobin.cpp
+++ src/matchmaking/tournament/roundrobin/roundrobin.cpp
@@ -16,7 +16,7 @@ RoundRobin::RoundRobin(const options::Tournament& tournament_config,
     // Initialize the SPRT test
     sprt_ = SPRT(tournament_options_.sprt.alpha, tournament_options_.sprt.beta,
                  tournament_options_.sprt.elo0, tournament_options_.sprt.elo1,
-                 tournament_options_.sprt.model);
+                 tournament_options_.sprt.model, tournament_options_.sprt.enabled);
 }
 
 void RoundRobin::start() {

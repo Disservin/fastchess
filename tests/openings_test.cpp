9dba339c112a32c4e80087d04ad888ef8413d751
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-17 04:35:14 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -11,6 +11,6 @@ TEST_SUITE("Openings") {
       tournament.seed = 123456789;
       tournament.opening.start = 3256;
       book::OpeningBook book(tournament);
-      book.book_[0] = 
+      CHECK(book.book_[0] == "r1bqkb1r/pp3pp1/2nppn2/7p/3NP1PP/2N5/PPP2P2/R1BQKBR1 w Qkq - 0 9");
 }
 

e18e957713ec33dffa8a181cf115982a9a882f4b
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-17 04:55:07 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -6,12 +6,17 @@
 using namespace fast_chess;
 
 TEST_SUITE("Openings") {
-      options::Tournament tournament;
-      tournament.rounds = 3000;
-      tournament.opening.file = "./data/openings.epd";
-      tournament.seed = 123456789;
-      tournament.opening.start = 3256;
-      book::OpeningBook book(tournament);
-      CHECK(book.book_[0] == "r1bqkb1r/pp3pp1/2nppn2/7p/3NP1PP/2N5/PPP2P2/R1BQKBR1 w Qkq - 0 9");
+    TEST_CASE("check epd openings") {
+        options::Tournament tournament;
+        tournament.rounds = 3000;
+        tournament.opening.file = "./data/openings.epd"; // Ensure the file path is set within opening
+        tournament.seed = 123456789;
+        tournament.opening.start = 3256;
+
+        book::OpeningBook book(tournament);
+
+
+        CHECK(book.book_[0] == "r1bqkb1r/pp3pp1/2nppn2/7p/3NP1PP/2N5/PPP2P2/R1BQKBR1 w Qkq - 0 9");
+    }
 }
 

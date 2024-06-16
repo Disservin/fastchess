7dba8a7607c84d6cd61024be495700ee6874f23b
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-17 05:19:51 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -9,14 +9,14 @@ TEST_SUITE("Openings") {
     TEST_CASE("check epd openings") {
         options::Tournament tournament;
         tournament.rounds = 3000;
-        tournament.opening.file = "./data/openings.epd"; // Ensure the file path is set within opening
+        tournament.opening.file = "./data/openings.epd";
         tournament.seed = 123456789;
         tournament.opening.start = 3256;
 
         auto book = book::OpeningBook(tournament);
-        auto epd = book.getEpdBook();
+        std::vector<std::string> epd = book.getEpdBook();
 
-        auto fen1 = epd[1];
+        std::string fen1 = epd[1];
 
         //CHECK(1 == 1);
         CHECK(fen1 == "r1bqkb1r/pp3pp1/2nppn2/7p/3NP1PP/2N5/PPP2P2/R1BQKBR1 w Qkq - 0 9");

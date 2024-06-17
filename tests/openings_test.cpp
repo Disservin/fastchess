3136aa97c80598717348a4afa0d34d26e0759220
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-18 03:02:05 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -48,10 +48,10 @@ TEST_SUITE("Openings") {
             opening[i] = book[book.fetchId()];
         }
 
-        CHECK(book_vector.size() == 100);
-        CHECK(book_vector.capacity() == 100);
+        CHECK(book_vector.size() == 10);
+        CHECK(book_vector.capacity() == 10);
         CHECK(opening[0].fen == "rn1qkb1r/1p3p1p/p2p1np1/2pP4/4P1b1/2N2N2/PP2QPPP/R1B1KB1R w KQkq - 2 9");
-        CHECK(opening[99].fen == "rnbqk2r/pp2p1bp/2pp1np1/5P2/3P4/2N3P1/PPP2PB1/R1BQK1NR w KQkq - 3 9");
+        CHECK(opening[9].fen == "rnbqk2r/pp2p1bp/2pp1np1/5P2/3P4/2N3P1/PPP2PB1/R1BQK1NR w KQkq - 3 9");
     }
 
     TEST_CASE("EPD 3") {

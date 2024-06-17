b8561f89b968931ff68f60e52ce5bea4f2deb519
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-18 02:47:40 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -18,7 +18,6 @@ TEST_SUITE("Openings") {
 
         auto book = book::OpeningBook(tournament, initial_matchcount);
         std::vector<std::string> book_vector = book.getEpdBook();
-        std::vector<std::optional<std::size_t>> index;
         std::vector<pgn::Opening> opening(tournament.rounds);
 
         for (int i=0; i < tournament.rounds; i++) {
@@ -26,7 +25,6 @@ TEST_SUITE("Openings") {
         }
 
         CHECK(book_vector.size() == 10);
-        CHECK(opening.size() == 10);
         CHECK(book_vector.capacity() == 10);
         CHECK(opening[0].fen == "rn1qkb1r/1p3p1p/p2p1np1/2pP4/4P1b1/2N2N2/PP2QPPP/R1B1KB1R w KQkq - 2 9");
         CHECK(opening[9].fen == "rnbqk2r/pp2p1bp/2pp1np1/5P2/3P4/2N3P1/PPP2PB1/R1BQK1NR w KQkq - 3 9");

49666cda2e04d7741ff2ca5c3b536d9fb76ce0d1
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-17 07:34:50 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -16,12 +16,12 @@ TEST_SUITE("Openings") {
         tournament.seed = 123456789;
 
         auto book = book::OpeningBook(tournament);
-        std::vector<std::string> epd = book.getEpdBook();
+        std::vector<std::string> pgn = book.getEpdBook();
 
-        CHECK(epd.size() == 10);
-        CHECK(epd.capacity() == 10);
-        CHECK(epd[0] == "1n1qkb1r/rp3ppp/p1p1pn2/2PpN2b/3PP3/1QN5/PP3PPP/R1B1KB1R w KQk - 0 9");
-        CHECK(epd[9] == "rn1qkb1r/4pp1p/3p1np1/2pP4/4P3/2N5/PP3PPP/R1BQ1KNR w kq - 0 9");
+        CHECK(pgn.size() == 10);
+        CHECK(pgn.capacity() == 10);
+        CHECK(pgn[0] == "1n1qkb1r/rp3ppp/p1p1pn2/2PpN2b/3PP3/1QN5/PP3PPP/R1B1KB1R w KQk - 0 9");
+        CHECK(pgn[9] == "rn1qkb1r/4pp1p/3p1np1/2pP4/4P3/2N5/PP3PPP/R1BQ1KNR w kq - 0 9");
     }
 
         TEST_CASE("check pgn openings") {

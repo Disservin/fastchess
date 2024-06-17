d340c34531a65035c5507a78130a3259ba1d7814
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-18 05:30:27 +0700

    Update opening_test.cpp

--- tests/opening_test.cpp
+++ tests/opening_test.cpp
@@ -14,8 +14,9 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.order  = OrderType::SEQUENTIAL;
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
+        int matchcount            = 0;
 
-        auto book = book::OpeningBook(tournament);
+        auto book = book::OpeningBook(tournament, matchcount);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
@@ -85,25 +86,26 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.order  = OrderType::SEQUENTIAL;
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
+        int matchcount            = 2;
 
-        auto book = book::OpeningBook(tournament);
+        auto book = book::OpeningBook(tournament, matchcount);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 0);
-        CHECK(book[id].fen == "5k2/3r1p2/1p3pp1/p2n3p/P6P/1PPR1PP1/3KN3/6b1 w - - 0 34");
-
-        id = book.fetchId();
-
-        REQUIRE(id.has_value());
-        CHECK(id.value() == 1);
         CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
-        CHECK(id.value() == 2);
+        CHECK(id.value() == 1);
         CHECK(book[id].fen == "8/p3kp1p/1p4p1/2r2b2/2BR3P/1P3P2/P4PK1/8 b - - 0 28");
+
+        id = book.fetchId();
+
+        REQUIRE(id.has_value());
+        CHECK(id.value() == 2);
+        CHECK(book[id].fen == "5k2/3r1p2/1p3pp1/p2n3p/P6P/1PPR1PP1/3KN3/6b1 w - - 0 34");
     }
 
     TEST_CASE("Test more openings than rounds") {
@@ -111,22 +113,23 @@ TEST_SUITE("Opening Book Test") {
         tournament.rounds         = 2;
         tournament.opening.file   = "tests/data/openings.epd";
         tournament.opening.format = FormatType::EPD;
-        tournament.opening.order  = OrderType::RANDOM;
+        tournament.opening.order  = OrderType::SEQUENTIAL;
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
+        int matchcount            = 0;
 
-        auto book = book::OpeningBook(tournament);
+        auto book = book::OpeningBook(tournament, matchcount);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
-        CHECK(id.value() == 55);
-        CHECK(book[id].fen == "rnbqk2r/1p3pbp/p2p1np1/2pP4/4P3/2N2N1P/PP3PP1/R1BQKB1R w KQkq - 1 9");
+        CHECK(id.value() == 0);
+        CHECK(book[id].fen == "r1b1kb1r/1p2pppp/p1np4/q5B1/3NP1n1/2N4P/PPP2PP1/R2QKB1R w KQkq - 1 9");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
-        CHECK(id.value() == 56);
-        CHECK(book[id].fen == "rnbqr1k1/pp1p1pp1/3b1n1p/2pP4/7B/2N2N2/PP2PPPP/R2QKB1R w KQ - 4 9");
+        CHECK(id.value() == 1);
+        CHECK(book[id].fen == "r1bqkb1r/1p3ppp/p1np1n2/4p3/3NPP2/2N1BQ2/PPP3PP/R3KB1R w KQkq - 0 9");
     }
 
     TEST_CASE("Test fewer openings than rounds random") {
@@ -137,19 +140,20 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.order  = OrderType::RANDOM;
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
+        int matchcount            = 2;
 
-        auto book = book::OpeningBook(tournament);
+        auto book = book::OpeningBook(tournament, matchcount);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 0);
-        CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
+        CHECK(book[id].fen == "8/p3kp1p/1p4p1/2r2b2/2BR3P/1P3P2/P4PK1/8 b - - 0 28");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 1);
-        CHECK(book[id].fen == "5k2/3r1p2/1p3pp1/p2n3p/P6P/1PPR1PP1/3KN3/6b1 w - - 0 34");
+        CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
     }
 
     TEST_CASE("Test equal openings to rounds random") {
@@ -160,19 +164,20 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.order  = OrderType::RANDOM;
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
+        int matchcount            = 2;
 
-        auto book = book::OpeningBook(tournament);
+        auto book = book::OpeningBook(tournament, matchcount);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 0);
-        CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
+        CHECK(book[id].fen == "8/p3kp1p/1p4p1/2r2b2/2BR3P/1P3P2/P4PK1/8 b - - 0 28");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 1);
-        CHECK(book[id].fen == "8/p3kp1p/1p4p1/2r2b2/2BR3P/1P3P2/P4PK1/8 b - - 0 28");
+        CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
 
         id = book.fetchId();
 
@@ -189,18 +194,19 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.order  = OrderType::RANDOM;
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
+        int matchcount            = 2;
 
-        auto book = book::OpeningBook(tournament);
+        auto book = book::OpeningBook(tournament, matchcount);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
-        CHECK(id.value() == 55);
-        CHECK(book[id].fen == "r1b1k1nr/ppq3pp/2n1p3/2ppPp2/3P4/P1P3Q1/2P2PPP/R1B1KBNR w KQkq - 2 9");
+        CHECK(id.value() == 0);
+        CHECK(book[id].fen == "rnbqkb1r/5ppp/p2p1n2/2pP4/Pp2P3/5NP1/1P3P1P/RNBQKB1R w KQkq - 0 9");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
-        CHECK(id.value() == 56);
-        CHECK(book[id].fen == "rnbqk2r/pp2p1bp/2pp1np1/5P2/3P4/2N3P1/PPP2PB1/R1BQK1NR w KQkq - 3 9");
+        CHECK(id.value() == 1);
+        CHECK(book[id].fen == "rn1qk2r/p2nbppp/bp2p3/2ppP3/3P4/2PB4/PP1NNPPP/R1BQ1RK1 w kq - 2 9");
     }
 }

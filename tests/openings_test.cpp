d2e03dccfcccd85e16681eed2a3b10cc628529bd
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-17 07:27:13 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -21,7 +21,7 @@ TEST_SUITE("Openings") {
         CHECK(epd.size() == 10);
         CHECK(epd.capacity() == 10);
         CHECK(epd[0] == "1n1qkb1r/rp3ppp/p1p1pn2/2PpN2b/3PP3/1QN5/PP3PPP/R1B1KB1R w KQk - 0 9");
-        CHECK(epd[9] == "rn1q1rk1/p2pbppp/bp3n2/2pp4/2P1P3/1QN2NP1/PP3P1P/R1B1KB1R w KQ - 0 9");
+        CHECK(epd[9] == "rn1qkb1r/4pp1p/3p1np1/2pP4/4P3/2N5/PP3PPP/R1BQ1KNR w kq - 0 9");
     }
 }
 

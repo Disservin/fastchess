8907ff26e7eabf07f6cbec565f26eea77c1a3ca9
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-17 07:34:04 +0700

    Update openings_test.cpp

--- tests/openings_test.cpp
+++ tests/openings_test.cpp
@@ -23,5 +23,23 @@ TEST_SUITE("Openings") {
         CHECK(epd[0] == "1n1qkb1r/rp3ppp/p1p1pn2/2PpN2b/3PP3/1QN5/PP3PPP/R1B1KB1R w KQk - 0 9");
         CHECK(epd[9] == "rn1qkb1r/4pp1p/3p1np1/2pP4/4P3/2N5/PP3PPP/R1BQ1KNR w kq - 0 9");
     }
+
+        TEST_CASE("check pgn openings") {
+        options::Tournament tournament;
+        tournament.rounds = 10;
+        tournament.opening.file = "tests/data/openings.pgn";
+        tournament.opening.format = FormatType::PGN;
+        tournament.opening.order = OrderType::RANDOM;
+        tournament.opening.start = 3256;
+        tournament.seed = 123456789;
+
+        auto book = book::OpeningBook(tournament);
+        std::vector<std::string> epd = book.getPgnBook();
+
+        CHECK(epd.size() == 10);
+        CHECK(epd.capacity() == 10);
+        CHECK(epd[0].fen == "1n1qkb1r/rp3ppp/p1p1pn2/2PpN2b/3PP3/1QN5/PP3PPP/R1B1KB1R w KQk - 0 9");
+        CHECK(epd[9].fen == "rn1qkb1r/4pp1p/3p1np1/2pP4/4P3/2N5/PP3PPP/R1BQ1KNR w kq - 0 9");
+    }
 }
 

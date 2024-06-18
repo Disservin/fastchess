c44c24bfa663b5adb8c0298f55f3e0600e32cd56
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-19 00:57:11 +0700

    Update opening_book.hpp

--- src/book/opening_book.hpp
+++ src/book/opening_book.hpp
@@ -19,15 +19,43 @@ class OpeningBook {
 
     // Fisher-Yates / Knuth shuffle
     void shuffle() {
-        const auto shuffle = [](auto& vec) {
-            for (std::size_t i = 0; i + 2 <= vec.size(); i++) {
-                auto rand     = util::random::mersenne_rand();
-                std::size_t j = i + (rand % (vec.size() - i));
-                std::swap(vec[i], vec[j]);
+        const auto shuffle = [this](auto& vec) {
+            if (order_ == OrderType::RANDOM) {
+                util::random::mersenne_rand.seed(seed_);
+                for (std::size_t i = 0; i + 2 <= vec.size(); i++) {
+                    auto rand     = util::random::mersenne_rand();
+                    std::size_t j = i + (rand % (vec.size() - i));
+                    std::swap(vec[i], vec[j]);
+                }
             }
         };
 
         std::visit(shuffle, book_);
+    }
+
+    // Rotates the book vector based on the starting offset
+    void rotate() {
+        const auto rotate = [this](auto& vec) {
+            std::rotate(vec.begin(), vec.begin() + (offset_ % vec.size()), vec.end());
+        };
+
+        std::visit(rotate, book_);
+    }
+
+   // Gets rid of unused openings within the book vector to reduce memory usage
+   void truncate() {
+        const auto truncate = [this](auto& vec) {
+            if (vec.size() > static_cast<std::size_t>(rounds_)) { vec.resize(rounds_); }
+        };
+
+        std::visit(truncate, book_);
+    }
+
+   // Shrink book vector
+   void shrink() {
+        const auto shrink = [](auto& vec) { vec.shrink_to_fit(); };
+
+        std::visit(shrink, book_);
     }
 
     [[nodiscard]] std::optional<std::size_t> fetchId() noexcept;
@@ -49,9 +77,10 @@ class OpeningBook {
     using epd_book = std::vector<std::string>;
     using pgn_book = std::vector<pgn::Opening>;
 
-    std::size_t opening_index_ = 0;
     std::size_t start_         = 0;
-    std::size_t matchcount_    = 0;
+    std::size_t opening_index_ = 0;
+    std::size_t offset_        = 0;
+    int rounds_;
     int games_;
     int plies_;
     OrderType order_;

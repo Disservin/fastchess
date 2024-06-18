305dc72c8134e39f5ae779dd0f886df68a93d8f5
Disservin <disservin.social@gmail.com>
2024-06-18 19:36:48 +0200

    fix(tests): make tests independent of order in which they are run

--- tests/opening_test.cpp
+++ tests/opening_test.cpp
@@ -13,7 +13,6 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.format = FormatType::EPD;
         tournament.opening.order  = OrderType::SEQUENTIAL;
         tournament.opening.start  = 3256;
-        tournament.seed           = 123456789;
 
         auto book = book::OpeningBook(tournament);
         auto id   = book.fetchId();
@@ -84,7 +83,6 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.format = FormatType::EPD;
         tournament.opening.order  = OrderType::SEQUENTIAL;
         tournament.opening.start  = 3256;
-        tournament.seed           = 123456789;
 
         auto book = book::OpeningBook(tournament);
         auto id   = book.fetchId();
@@ -111,22 +109,21 @@ TEST_SUITE("Opening Book Test") {
         tournament.rounds         = 2;
         tournament.opening.file   = "tests/data/openings.epd";
         tournament.opening.format = FormatType::EPD;
-        tournament.opening.order  = OrderType::RANDOM;
+        tournament.opening.order  = OrderType::SEQUENTIAL;
         tournament.opening.start  = 3256;
-        tournament.seed           = 123456789;
 
         auto book = book::OpeningBook(tournament);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 55);
-        CHECK(book[id].fen == "rnbqk2r/1p3pbp/p2p1np1/2pP4/4P3/2N2N1P/PP3PP1/R1BQKB1R w KQkq - 1 9");
+        CHECK(book[id].fen == "r1b1kb1r/1p2pppp/p1np4/q5B1/3NP1n1/2N4P/PPP2PP1/R2QKB1R w KQkq - 1 9");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 56);
-        CHECK(book[id].fen == "rnbqr1k1/pp1p1pp1/3b1n1p/2pP4/7B/2N2N2/PP2PPPP/R2QKB1R w KQ - 4 9");
+        CHECK(book[id].fen == "r1bqkb1r/1p3ppp/p1np1n2/4p3/3NPP2/2N1BQ2/PPP3PP/R3KB1R w KQkq - 0 9");
     }
 
     TEST_CASE("Test fewer openings than rounds random") {
@@ -138,18 +135,20 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
 
+        util::random::mersenne_rand.seed(tournament.seed);
+
         auto book = book::OpeningBook(tournament);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 0);
-        CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
+        CHECK(book[id].fen == "5k2/3r1p2/1p3pp1/p2n3p/P6P/1PPR1PP1/3KN3/6b1 w - - 0 34");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 1);
-        CHECK(book[id].fen == "5k2/3r1p2/1p3pp1/p2n3p/P6P/1PPR1PP1/3KN3/6b1 w - - 0 34");
+        CHECK(book[id].fen == "8/p3kp1p/1p4p1/2r2b2/2BR3P/1P3P2/P4PK1/8 b - - 0 28");
     }
 
     TEST_CASE("Test equal openings to rounds random") {
@@ -161,12 +160,14 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
 
+        util::random::mersenne_rand.seed(tournament.seed);
+
         auto book = book::OpeningBook(tournament);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 0);
-        CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
+        CHECK(book[id].fen == "5k2/3r1p2/1p3pp1/p2n3p/P6P/1PPR1PP1/3KN3/6b1 w - - 0 34");
 
         id = book.fetchId();
 
@@ -178,7 +179,7 @@ TEST_SUITE("Opening Book Test") {
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 2);
-        CHECK(book[id].fen == "5k2/3r1p2/1p3pp1/p2n3p/P6P/1PPR1PP1/3KN3/6b1 w - - 0 34");
+        CHECK(book[id].fen == "5k2/5p2/4B2p/r5pn/4P3/5PPP/2NR2K1/8 b - - 0 59");
     }
 
     TEST_CASE("Test more openings than rounds random") {
@@ -190,17 +191,19 @@ TEST_SUITE("Opening Book Test") {
         tournament.opening.start  = 3256;
         tournament.seed           = 123456789;
 
+        util::random::mersenne_rand.seed(tournament.seed);
+
         auto book = book::OpeningBook(tournament);
         auto id   = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 55);
-        CHECK(book[id].fen == "r1b1k1nr/ppq3pp/2n1p3/2ppPp2/3P4/P1P3Q1/2P2PPP/R1B1KBNR w KQkq - 2 9");
+        CHECK(book[id].fen == "1n1qkb1r/rp3ppp/p1p1pn2/2PpN2b/3PP3/1QN5/PP3PPP/R1B1KB1R w KQk - 0 9");
 
         id = book.fetchId();
 
         REQUIRE(id.has_value());
         CHECK(id.value() == 56);
-        CHECK(book[id].fen == "rnbqk2r/pp2p1bp/2pp1np1/5P2/3P4/2N3P1/PPP2PB1/R1BQK1NR w KQkq - 3 9");
+        CHECK(book[id].fen == "rnbqkb1r/5ppp/p2p1n2/2pP4/Pp2P3/5NP1/1P3P1P/RNBQKB1R w KQkq - 0 9");
     }
 }

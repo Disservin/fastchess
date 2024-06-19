aff613707291713277573aac6d16024b2b7a8b98
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-20 03:52:15 +0700

    Update rand.hpp

--- src/util/rand.hpp
+++ src/util/rand.hpp
@@ -4,4 +4,16 @@
 
 namespace fast_chess::util::random {
 inline std::mt19937_64 mersenne_rand;
+
+// Function to generate a random integer between x and y (inclusive)
+inline uint64_t rand_int(uint64_t x, uint64_t y) {
+    std::uniform_int_distribution<uint64_t> dist(x, y);
+    return dist(mersenne_rand);
+}
+
+// Function to generate a random floating-point number between x and y (inclusive)
+inline double rand_double(double x, double y) {
+    std::uniform_real_distribution<double> dist(x, y);
+    return dist(mersenne_rand);
+}
 }  // namespace fast_chess::util::random

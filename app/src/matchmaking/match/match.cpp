f88f2cce00525064865afd3b2321e11d25fa1eff
disservin <disservin.social@gmail.com>
2024-06-23 14:28:56 +0200

    fix: illegal moves when engine times out (#490)

--- app/src/matchmaking/match/match.cpp
+++ app/src/matchmaking/match/match.cpp
@@ -203,46 +203,33 @@ bool Match::playMove(Player& us, Player& them) {
 
     const auto elapsed_millis = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
 
-    // Illegal move
     const auto best_move = us.engine.bestmove();
     const auto move      = best_move ? uci::uciToMove(board_, *best_move) : Move::NO_MOVE;
     const auto legal     = isLegal(move);
 
+    const auto timeout = !us.updateTime(elapsed_millis);
+
     addMoveData(us, elapsed_millis, legal);
 
-    if (!legal) {
-        us.setLost();
-        them.setWon();
-
-        data_.termination = MatchTermination::ILLEGAL_MOVE;
-        data_.reason      = name + Match::ILLEGAL_MSG;
-
-        Logger::warn<true>("Warning; Illegal move {} played by {}", best_move ? *best_move : "<none>", name);
+    // there are two reasons why best_move could be empty
+    // 1. the engine crashed
+    // 2. the engine did not respond in time
+    // we report a loss on time when the engine didnt respond in time
+    // and otherwise an illegal move
+    if (best_move == std::nullopt) {
+        // Time forfeit
+        if (timeout) {
+            setEngineTimeoutStatus(us, them);
+        } else {
+            setEngineIllegalMoveStatus(us, them, best_move);
+        }
 
         return false;
     }
 
-    // Time forfeit
-    if (!us.updateTime(elapsed_millis)) {
-        us.setLost();
-        them.setWon();
-
-        data_.termination = MatchTermination::TIMEOUT;
-        data_.reason      = name + Match::TIMEOUT_MSG;
-
-        Logger::warn<true>("Warning; Engine {} loses on time", name);
-
-        // we send a stop command to the engine to prevent it from thinking
-        // and wait for a bestmove to appear
-
-        us.engine.writeEngine("stop");
-
-        if (!us.engine.outputIncludesBestmove()) {
-            // wait for bestmove, indefinitely
-            // is this a good idea? what if the engine is stuck?
-            us.engine.readEngine("bestmove", 0ms);
-        }
-
+    // illegal move
+    if (!legal) {
+        setEngineIllegalMoveStatus(us, them, best_move);
         return false;
     }
 
@@ -275,6 +262,40 @@ void Match::setEngineCrashStatus(Player& loser, Player& winner) {
 
     data_.termination = MatchTermination::DISCONNECT;
     data_.reason      = loser.engine.getConfig().name + Match::DISCONNECT_MSG;
+}
+
+void Match::setEngineTimeoutStatus(Player& loser, Player& winner) {
+    loser.setLost();
+    winner.setWon();
+
+    const auto name = loser.engine.getConfig().name;
+
+    data_.termination = MatchTermination::TIMEOUT;
+    data_.reason      = name + Match::TIMEOUT_MSG;
+
+    Logger::warn<true>("Warning; Engine {} loses on time", name);
+
+    // we send a stop command to the engine to prevent it from thinking
+    // and wait for a bestmove to appear
+
+    loser.engine.writeEngine("stop");
+
+    if (!loser.engine.outputIncludesBestmove()) {
+        // wait 10 seconds for the bestmove to appear
+        loser.engine.readEngine("bestmove", 1000ms * 10);
+    }
+}
+
+void Match::setEngineIllegalMoveStatus(Player& loser, Player& winner, const std::optional<std::string>& best_move) {
+    loser.setLost();
+    winner.setWon();
+
+    const auto name = loser.engine.getConfig().name;
+
+    data_.termination = MatchTermination::ILLEGAL_MOVE;
+    data_.reason      = name + Match::ILLEGAL_MSG;
+
+    Logger::warn<true>("Warning; Illegal move {} played by {}", best_move ? *best_move : "<none>", name);
 }
 
 bool Match::isUciMove(const std::string& move) noexcept {

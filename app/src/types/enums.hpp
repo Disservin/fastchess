2385a259609a4ede5b41571e5fa7aed615413e02
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-07-15 22:38:30 +0700

    Update enums.hpp

--- app/src/types/enums.hpp
+++ app/src/types/enums.hpp
@@ -3,7 +3,7 @@
 namespace fast_chess {
 
 enum class NotationType { SAN, LAN, UCI };
-enum class OrderType { RANDOM, SEQUENTIAL };
+enum class OrderType { RANDOM, SEQUENTIAL, NONE };
 enum class FormatType { EPD, PGN, NONE };
 enum class VariantType { STANDARD, FRC };
 enum class OutputType {

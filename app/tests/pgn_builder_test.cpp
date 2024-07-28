bd73bc3f5f55c975e57614d0c8ac2c77cae6086d
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-07-28 19:18:19 +0700

    Update pgn_builder_test.cpp

--- app/tests/pgn_builder_test.cpp
+++ app/tests/pgn_builder_test.cpp
@@ -201,8 +201,8 @@ Nf6 {+10.15/18 1.821s engine1 got checkmated} 0-1
 [SetUp "1"]
 [FEN "r2qk2r/1bpp2pp/n3pn2/p2P1p2/1bP5/2N1BNP1/1PQ1PPBP/R3K2R b KQkq - 0 1"]
 [PlyCount "3"]
-[WhiteTimeControl "1+5"]
-[BlackTimeControl "0+5"]
+[WhiteTimeControl "0.001+0.005"]
+[BlackTimeControl "0+0.005"]
 
 1... O-O {+1.00/15 1.321s} 2. O-O {+1.23/15 0.430s} Nc5 {+1.45/16 0.310s aborted} *
 

b5c56db41e60800cf9a64cdf754a6752cc0f14e7
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-08-03 19:46:00 +0700

    Update match.hpp

--- app/src/matchmaking/match/match.hpp
+++ app/src/matchmaking/match/match.hpp
@@ -113,13 +113,14 @@ class Match {
     // returns the match data, only valid after the match has finished
     [[nodiscard]] const MatchData& get() const { return data_; }
 
-    [[nodiscard]] bool isCrashOrDisconnect() const noexcept { return crash_or_disconnect_; }
+    [[nodiscard]] bool isStallOrDisconnect() const noexcept { return stall_or_disconnect_; }
 
    private:
     // returns the reason and the result of the game, different order than chess lib function
     [[nodiscard]] std::pair<chess::GameResultReason, chess::GameResult> isGameOver() const;
 
     void setEngineCrashStatus(Player& loser, Player& winner);
+    void setEngineStallStatus(Player& loser, Player& winner);
     void setEngineTimeoutStatus(Player& loser, Player& winner);
     void setEngineIllegalMoveStatus(Player& loser, Player& winner, const std::optional<std::string>& best_move);
 
@@ -166,7 +167,7 @@ class Match {
     // is either startpos or the fen of the opening
     std::string start_position_;
 
-    bool crash_or_disconnect_ = false;
+    bool stall_or_disconnect_ = false;
 
     inline static constexpr char INSUFFICIENT_MSG[]     = "Draw by insufficient mating material";
     inline static constexpr char REPETITION_MSG[]       = "Draw by 3-fold repetition";

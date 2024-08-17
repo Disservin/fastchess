00e7cbaddd35870a380e1fe2d158a727840789a9
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-08-18 00:05:48 +0700

    Update output_fastchess.hpp

--- app/src/matchmaking/output/output_fastchess.hpp
+++ app/src/matchmaking/output/output_fastchess.hpp
@@ -107,7 +107,7 @@ class Fastchess : public IOutput {
 
     void startGame(const GamePair<EngineConfiguration, EngineConfiguration>& configs, std::size_t current_game_count,
                    std::size_t max_game_count) override {
-        auto fmt = fmt::format("Started game {} of {} ({} vs {})\n", current_game_count, max_game_count,
+        auto fmt = fmt::format("Started game {} of {} ({} vs {})", current_game_count, max_game_count,
                                configs.white.name, configs.black.name);
 
         Logger::info(fmt);
@@ -115,7 +115,7 @@ class Fastchess : public IOutput {
 
     void endGame(const GamePair<EngineConfiguration, EngineConfiguration>& configs, const Stats& stats,
                  const std::string& annotation, std::size_t id) override {
-        auto fmt = fmt::format("Finished game {} ({} vs {}): {} {{{}}}\n", id, configs.white.name, configs.black.name,
+        auto fmt = fmt::format("Finished game {} ({} vs {}): {} {{{}}}", id, configs.white.name, configs.black.name,
                                formatStats(stats), annotation);
 
         Logger::info(fmt);

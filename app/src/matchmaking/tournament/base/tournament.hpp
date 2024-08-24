b080cfed6b0a521d3dbd364a8ed3ae6cb915c4bf
disservin <disservin.social@gmail.com>
2024-08-24 14:58:07 +0200

    refactor: the opening book return type (#683)

--- app/src/matchmaking/tournament/base/tournament.hpp
+++ app/src/matchmaking/tournament/base/tournament.hpp
@@ -62,7 +62,7 @@ class BaseTournament {
 
     // play one game and write it to the pgn file
     void playGame(const GamePair<EngineConfiguration, EngineConfiguration> &configs, start_callback start,
-                  finished_callback finish, const pgn::Opening &opening, std::size_t round_id, std::size_t game_id);
+                  finished_callback finish, const book::Opening &opening, std::size_t round_id, std::size_t game_id);
 
     util::CachePool<engine::UciEngine, std::string> engine_cache_ = util::CachePool<engine::UciEngine, std::string>();
     util::ThreadPool pool_                                        = util::ThreadPool(1);

a742c79606d6db42a92ab2a594ce2a4f862afb68
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-08-31 16:32:20 +0700

    refactor: change pgn mate score format from moves to plies

--- app/src/matchmaking/match/match.cpp
+++ app/src/matchmaking/match/match.cpp
@@ -53,7 +53,9 @@ void Match::addMoveData(const Player& player, int64_t measured_time_ms, int64_t 
         ss << (move_data.score >= 0 ? '+' : '-');
         ss << std::fixed << std::setprecision(2) << (float(std::abs(move_data.score)) / 100);
     } else if (score_type == engine::ScoreType::MATE) {
-        ss << (move_data.score > 0 ? "+M" : "-M") << std::to_string(std::abs(move_data.score));
+        uint64_t plies = board_.sideToMove() == Chess::Color::WHITE ? std::abs(move_data.score) * 2 
+                                                                    : std::abs(move_data.score) * 2 - 1;
+        ss << (move_data.score > 0 ? "+M" : "-M") << std::to_string(plies);
     } else {
         ss << "ERR";
     }

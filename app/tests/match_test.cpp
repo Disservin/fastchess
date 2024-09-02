c328ef9a64cf4b04ba47ede2f3eb4c9af22d7022
Disservin <disservin.social@gmail.com>
2024-09-02 11:52:08 +0200

    fix

--- app/tests/match_test.cpp
+++ app/tests/match_test.cpp
@@ -10,6 +10,8 @@ TEST_SUITE("Match Test") {
         CHECK(Match::convertScoreToString(-100, engine::ScoreType::CP) == "-1.00");
         CHECK(Match::convertScoreToString(100, engine::ScoreType::MATE) == "+M199");
         CHECK(Match::convertScoreToString(-100, engine::ScoreType::MATE) == "-M200");
+        CHECK(Match::convertScoreToString(1, engine::ScoreType::MATE) == "+M1");
+        CHECK(Match::convertScoreToString(-1, engine::ScoreType::MATE) == "-M2");
     }
 }
 }  // namespace fastchess

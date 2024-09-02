a6308783cfe025cc4dbfec332488d9e4bd46c37e
Disservin <disservin.social@gmail.com>
2024-09-02 11:56:35 +0200

    add test

---
+++ app/tests/match_test.cpp
@@ -0,0 +1,15 @@
+#include <matchmaking/match/match.hpp>
+
+#include "doctest/doctest.hpp"
+
+namespace fastchess {
+TEST_SUITE("Match Test") {
+    TEST_CASE("convertScoreToString") {
+        CHECK(Match::convertScoreToString(0, engine::ScoreType::CP) == "+0.00");
+        CHECK(Match::convertScoreToString(100, engine::ScoreType::CP) == "+1.00");
+        CHECK(Match::convertScoreToString(-100, engine::ScoreType::CP) == "-1.00");
+        CHECK(Match::convertScoreToString(100, engine::ScoreType::MATE) == "+M199");
+        CHECK(Match::convertScoreToString(-100, engine::ScoreType::MATE) == "-M200");
+    }
+}
+}  // namespace fastchess

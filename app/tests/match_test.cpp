488081b401647a48537516408cc411cdcea31aef
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-09-03 17:19:36 +0700

    Update match_test.cpp

--- app/tests/match_test.cpp
+++ app/tests/match_test.cpp
@@ -5,7 +5,7 @@
 namespace fastchess {
 TEST_SUITE("Match Test") {
     TEST_CASE("convertScoreToString") {
-        CHECK(Match::convertScoreToString(0, engine::ScoreType::CP) == "+0.00");
+        CHECK(Match::convertScoreToString(0, engine::ScoreType::CP) == "0.00");
         CHECK(Match::convertScoreToString(100, engine::ScoreType::CP) == "+1.00");
         CHECK(Match::convertScoreToString(-100, engine::ScoreType::CP) == "-1.00");
         CHECK(Match::convertScoreToString(100, engine::ScoreType::MATE) == "+M199");

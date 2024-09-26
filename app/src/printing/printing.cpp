46b44759d30030af94f14490b2660ff56a94512d
Disservin <disservin.social@gmail.com>
2024-09-26 23:03:00 +0200

    fix(windows): set terminal output code

--- app/src/printing/printing.cpp
+++ app/src/printing/printing.cpp
@@ -22,6 +22,33 @@ void setTerminalOutput() {
         std::cerr << "Failed to set console output code page. Error code: " << error << std::endl;
         return;
     }
+
+    if (!SetConsoleCP(65001)) {
+        DWORD error = GetLastError();
+        std::cerr << "Failed to set console input code page. Error code: " << error << std::endl;
+        return;
+    }
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hConsole == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error: Unable to get console handle." << std::endl;
+        return;
+    }
+
+    DWORD consoleMode;
+    if (!GetConsoleMode(hConsole, &consoleMode)) {
+        DWORD error = GetLastError();
+        std::cerr << "Failed to get console mode. Error code: " << error << std::endl;
+        return;
+    }
+
+    // Add the ENABLE_VIRTUAL_TERMINAL_PROCESSING flag to support ANSI colors
+    consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hConsole, consoleMode)) {
+        DWORD error = GetLastError();
+        std::cerr << "Failed to set console mode. Error code: " << error << std::endl;
+        return;
+    }
 #endif
 }
 

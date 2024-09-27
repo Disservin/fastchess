87f6a966db99ebdd40c1b85167a060f2ef7f472c
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-09-28 05:58:04 +0700

    Update timecontrol_test.cpp

--- app/tests/timecontrol_test.cpp
+++ app/tests/timecontrol_test.cpp
@@ -15,19 +15,19 @@ TEST_SUITE("TimeControl") {
         TimeControl tc(limits);
 
         CHECK(tc.updateTime(5555) == true);
-        CHECK(tc.getTimeLeft() == 4645);
+        CHECK(tc.getTimeLeft() == tc.time + tc.increment - 5555 + tc.increment);
         CHECK(tc.getMovesLeft() == 2);
 
         CHECK(tc.updateTime(4745) == true);
-        CHECK(tc.getTimeLeft() == 100);
+        CHECK(tc.getTimeLeft() == tc.increment);
         CHECK(tc.getMovesLeft() == 1);
 
         CHECK(tc.updateTime(50) == true);
-        CHECK(tc.getTimeLeft() == 10150);
+        CHECK(tc.getTimeLeft() == tc.time + tc.increment - 50 + tc.increment);
         CHECK(tc.getMovesLeft() == 3);
 
         CHECK(tc.updateTime(10251) == false);
-        CHECK(tc.getTimeLeft() == -101);
+        CHECK(tc.getTimeLeft() == tc.time + tc.increment - 10251);
         CHECK(tc.getMovesLeft() == 2);
     }
 
@@ -38,14 +38,15 @@ TEST_SUITE("TimeControl") {
 
         TimeControl tc(limits);
 
-        CHECK(tc.updateTime(5199) == true);
-        CHECK(tc.getTimeLeft() == 5000);
 
-        CHECK(tc.updateTime(5200) == true);
-        CHECK(tc.getTimeLeft() == 5000);
+        CHECK(tc.updateTime(tc.fixed_time + tc.timemargin - 1) == true);
+        CHECK(tc.getTimeLeft() == tc.fixed_time);
+        
+        CHECK(tc.updateTime(tc.fixed_time + tc.timemargin) == true);
+        CHECK(tc.getTimeLeft() == tc.fixed_time);
 
-        CHECK(tc.updateTime(5201) == false);
-        CHECK(tc.getTimeLeft() == -201);
+        CHECK(tc.updateTime(tc.fixed_time + tc.timemargin + 1) == false);
+        CHECK(tc.getTimeLeft() == tc.fixed_time - (tc.fixed_time + tc.timemargin + 1));
     }
 
     TEST_CASE("Fixed depth/nodes") {

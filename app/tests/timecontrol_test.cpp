a072b455458dd7a066477f3d7dcf684d50279769
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-09-28 04:04:42 +0700

    Update timecontrol_test.cpp

--- app/tests/timecontrol_test.cpp
+++ app/tests/timecontrol_test.cpp
@@ -1,1 +1,31 @@
+#include <time/timecontrol.hpp>
 
+#include "doctest/doctest.hpp"
+
+using namespace fastchess;
+
+TEST_SUITE("Elo Model") {
+    TEST_CASE("moves/time+increment") {
+        TimeControl tc;
+        tc.setMoves(3);
+        tc.setTime(10000);
+        tc.setIncrement(100);
+        tc.setTimemargin(100);
+
+        CHECK(tc.updateTime(5555) == true);
+        CHECK(tc.getTimeLeft() == 4545);
+        CHECK(tc.getMovesLeft() == 2);
+
+        CHECK(tc.updateTime(4645) == true);
+        CHECK(tc.getTimeLeft() == 100);
+        CHECK(tc.getMovesLeft() == 1);
+
+        CHECK(tc.updateTime(50) == true);
+        CHECK(tc.getTimeLeft() == 10150);
+        CHECK(tc.getMovesLeft() == 3);
+
+        CHECK(tc.updateTime(10251) == false);
+        CHECK(tc.getTimeLeft() == -1);
+        CHECK(tc.getMovesLeft() == 2);
+    }
+}

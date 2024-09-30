a2a9dabfc15237fb90a309b07c21a1f23625e125
disservin <disservin.social@gmail.com>
2024-09-30 23:58:03 +0200

    fix: dont add movedata if we have a timeout (#723)

--- app/src/matchmaking/match/match.cpp
+++ app/src/matchmaking/match/match.cpp
@@ -253,7 +253,9 @@ bool Match::playMove(Player& us, Player& them) {
     const auto timeout  = !us.updateTime(elapsed_millis);
     const auto timeleft = us.getTimeControl().getTimeLeft();
 
-    addMoveData(us, elapsed_millis, timeleft, legal && isUciMove(best_move.value()));
+    if (best_move) {
+        addMoveData(us, elapsed_millis, timeleft, legal && isUciMove(best_move.value()));
+    }
 
     // there are two reasons why best_move could be empty
     // 1. the engine crashed

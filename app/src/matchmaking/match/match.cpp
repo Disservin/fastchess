d579b62a6212b528e6dfb9f49cdb17a11e541a1e
disservin <disservin.social@gmail.com>
2025-01-04 21:09:19 +0100

    fix: add gameover check to pv verification (#755)
    add additional gameover check when verifying pv as weird things can happen when movegen is called after the game is over, or an engine not respecting half move counters for example

--- app/src/matchmaking/match/match.cpp
+++ app/src/matchmaking/match/match.cpp
@@ -457,9 +457,15 @@ void Match::verifyPvLines(const Player& us) {
         Movelist moves;
 
         while (it_start != it_end) {
-            movegen::legalmoves(moves, board);
+            moves.clear();
 
-            if (std::find(moves.begin(), moves.end(), uci::uciToMove(board, *it_start)) == moves.end()) {
+            const auto gameover = board.isGameOver().second != GameResult::NONE;
+
+            if (!gameover) {
+                movegen::legalmoves(moves, board);
+            }
+
+            if (gameover || std::find(moves.begin(), moves.end(), uci::uciToMove(board, *it_start)) == moves.end()) {
                 auto fmt      = fmt::format("Warning; Illegal pv move {} pv; {}", *it_start, info);
                 auto position = fmt::format("position {}", startpos == "startpos" ? "startpos" : ("fen " + startpos));
                 auto fmt2     = fmt::format("From; {} moves {}", position, str_utils::join(uci_moves, " "));

c637ba33fd28aa4a6f98621845de6582e6922e74
Disservin <disservin.social@gmail.com>
2025-01-04 22:45:05 +0100

    fix?

--- app/compatability/spawn-check.mk
+++ app/compatability/spawn-check.mk
@@ -1,12 +1,12 @@
 HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR := $(shell \
-    echo '#include <spawn.h>' > test.c && \
+    echo "#include <spawn.h>" > test.c && \
     echo 'int main(){return posix_spawn_file_actions_addchdir(0,0);}' >> test.c && \
     if $(CC) test.c -o test 2>/dev/null; then echo 1; else echo 0; fi && \
     rm -f test.c test)
 
 ifeq ($(HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR),0)
 	HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR_NP := $(shell \
-		echo '#include <spawn.h>' > test.c && \
+		echo "#include <spawn.h>" > test.c && \
 		echo 'int main(){return posix_spawn_file_actions_addchdir_np(0,0);}' >> test.c && \
 		if $(CC) test.c -o test 2>/dev/null; then echo 1; else echo 0; fi && \
 		rm -f test.c test)

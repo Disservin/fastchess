853834addd6464e583319be0e498866a97f425f5
Disservin <disservin.social@gmail.com>
2025-01-04 22:46:53 +0100

    try this

--- app/compatability/spawn-check.mk
+++ app/compatability/spawn-check.mk
@@ -1,13 +1,15 @@
 HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR := $(shell \
-    echo "#include <spawn.h>" > test.c && \
-    echo 'int main(){return posix_spawn_file_actions_addchdir(0,0);}' >> test.c && \
-    if $(CC) test.c -o test 2>/dev/null; then echo 1; else echo 0; fi && \
-    rm -f test.c test)
+    printf '%s\n%s\n' \
+        '#include <spawn.h>' \
+        'int main(){return posix_spawn_file_actions_addchdir(0,0);}' > test.c \
+    && if $(CC) test.c -o test 2>/dev/null; then echo 1; else echo 0; fi \
+    && rm -f test.c test)
 
 ifeq ($(HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR),0)
 	HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR_NP := $(shell \
-		echo "#include <spawn.h>" > test.c && \
-		echo 'int main(){return posix_spawn_file_actions_addchdir_np(0,0);}' >> test.c && \
-		if $(CC) test.c -o test 2>/dev/null; then echo 1; else echo 0; fi && \
-		rm -f test.c test)
+		printf '%s\n%s\n' \
+			'#include <spawn.h>' \
+			'int main(){return posix_spawn_file_actions_addchdir_np(0,0);}' > test.c \
+		&& if $(CC) test.c -o test 2>/dev/null; then echo 1; else echo 0; fi \
+		&& rm -f test.c test)
 endif

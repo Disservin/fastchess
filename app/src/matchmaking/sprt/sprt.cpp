266b35ad78f6ce9b6611f37047e5d4689935b6b1
87 <dev@87flowers.com>
2025-01-29 19:41:13 +0000

    improve llr algorithm

--- app/src/matchmaking/sprt/sprt.cpp
+++ app/src/matchmaking/sprt/sprt.cpp
@@ -1,10 +1,13 @@
 #include <matchmaking/sprt/sprt.hpp>
 
+#include <algorithm>
+#include <cassert>
 #include <cmath>
 #include <iomanip>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <tuple>
 
 #include <core/logger/logger.hpp>
 #include <matchmaking/stats.hpp>
@@ -66,6 +69,11 @@ void SPRT::isValid(double alpha, double beta, double elo0, double elo1, std::str
     }
 }
 
+static double regularize(int value) {
+    if (value == 0) return 1e-3;
+    return value;
+}
+
 double SPRT::getLLR(const Stats& stats, bool penta) const noexcept {
     if (penta)
         return getLLR(stats.penta_WW, stats.penta_WD, stats.penta_WL, stats.penta_DD, stats.penta_LD, stats.penta_LL);
@@ -76,106 +84,224 @@ double SPRT::getLLR(const Stats& stats, bool penta) const noexcept {
 double SPRT::getLLR(int win, int draw, int loss) const noexcept {
     if (!enabled_) return 0.0;
 
-    const bool regularize = ((win == 0) + (draw == 0) + (loss == 0)) >= 2;
-    const double games    = win + draw + loss + 1.5 * regularize;
-
-    if (games == 0) return 0.0;
-
-    const double W = (win + 0.5 * regularize) / games;
-    const double D = (draw + 0.5 * regularize) / games;
-    const double L = (loss + 0.5 * regularize) / games;
-
-    double score0;
-    double score1;
+    double L     = regularize(loss);
+    double D     = regularize(draw);
+    double W     = regularize(win);
+    double total = L + D + W;
+    std::array<double, 3> probs{L / total, D / total, W / total};
 
     if (model_ == "normalized") {
-        const double score    = W + 0.5 * D;
-        const double W_dev    = W * std::pow((1 - score), 2);
-        const double D_dev    = D * std::pow((0.5 - score), 2);
-        const double L_dev    = L * std::pow((0 - score), 2);
-        const double variance = W_dev + D_dev + L_dev;
-        if (variance == 0) return 0.0;
-        score0 = neloToScoreWDL(elo0_, variance);
-        score1 = neloToScoreWDL(elo1_, variance);
+        double t0 = elo0_ / (800.0 / std::log(10));
+        double t1 = elo1_ / (800.0 / std::log(10));
+        return getLLR_normalized(total, {0.0, 0.5, 1.0}, probs, t0, t1);
     } else if (model_ == "bayesian") {
         if (win == 0 || loss == 0) return 0.0;
-        const double drawelo = 200 * std::log10((1 - L) / L * (1 - W) / W);
-        score0               = bayeseloToScore(elo0_, drawelo);
-        score1               = bayeseloToScore(elo1_, drawelo);
+        double L       = probs[0];
+        double W       = probs[2];
+        double drawelo = 200 * std::log10((1 - L) / L * (1 - W) / W);
+        double score0  = bayeseloToScore(elo0_, drawelo);
+        double score1  = bayeseloToScore(elo1_, drawelo);
+        return getLLR_logistic(total, {0.0, 0.5, 1.0}, probs, score0, score1);
     } else {
-        score0 = leloToScore(elo0_);
-        score1 = leloToScore(elo1_);
+        double score0 = leloToScore(elo0_);
+        double score1 = leloToScore(elo1_);
+        return getLLR_logistic(total, {0.0, 0.5, 1.0}, probs, score0, score1);
     }
-
-    const double W_dev0    = W * std::pow((1 - score0), 2);
-    const double D_dev0    = D * std::pow((0.5 - score0), 2);
-    const double L_dev0    = L * std::pow((0 - score0), 2);
-    const double variance0 = W_dev0 + D_dev0 + L_dev0;
-    const double W_dev1    = W * std::pow((1 - score1), 2);
-    const double D_dev1    = D * std::pow((0.5 - score1), 2);
-    const double L_dev1    = L * std::pow((0 - score1), 2);
-    const double variance1 = W_dev1 + D_dev1 + L_dev1;
-
-    if (variance0 == 0 || variance1 == 0) return 0.0;
-
-    // For more information: http://hardy.uhasselt.be/Fishtest/support_MLE_multinomial.pdf
-    return 0.5 * games * std::log(variance0 / variance1);
 }
 
 double SPRT::getLLR(int penta_WW, int penta_WD, int penta_WL, int penta_DD, int penta_LD, int penta_LL) const noexcept {
     if (!enabled_) return 0.0;
 
-    const bool regularize =
-        ((penta_WW == 0) + (penta_WD == 0) + ((penta_WL + penta_DD) == 0) + (penta_LD == 0) + (penta_LL == 0)) >= 4;
-    const double pairs = penta_WW + penta_WD + penta_WL + penta_DD + penta_LD + penta_LL + 2.5 * regularize;
-
-    if (pairs == 0) return 0.0;
-
-    const double WW = (penta_WW + 0.5 * regularize) / pairs;
-    const double WD = (penta_WD + 0.5 * regularize) / pairs;
-    const double WL = (penta_WL + 0.25 * regularize) / pairs;
-    const double DD = (penta_DD + 0.25 * regularize) / pairs;
-    const double LD = (penta_LD + 0.5 * regularize) / pairs;
-    const double LL = (penta_LL + 0.5 * regularize) / pairs;
-
-    double score0;
-    double score1;
+    double LL    = regularize(penta_LL);
+    double LD    = regularize(penta_LD);
+    double WL_DD = regularize(penta_DD + penta_WL);
+    double WD    = regularize(penta_WD);
+    double WW    = regularize(penta_WW);
+    double total = WW + WD + WL_DD + LD + LL;
+    std::array<double, 5> probs{
+        LL / total, LD / total, WL_DD / total, WD / total, WW / total,
+    };
 
     if (model_ == "normalized") {
-        const double score    = WW + 0.75 * WD + 0.5 * (WL + DD) + 0.25 * LD;
-        const double WW_dev   = WW * std::pow((1 - score), 2);
-        const double WD_dev   = WD * std::pow((0.75 - score), 2);
-        const double WLDD_dev = (WL + DD) * std::pow((0.5 - score), 2);
-        const double LD_dev   = LD * std::pow((0.25 - score), 2);
-        const double LL_dev   = LL * std::pow((0 - score), 2);
-        const double variance = WW_dev + WD_dev + WLDD_dev + LD_dev + LL_dev;
-
-        if (variance == 0) return 0.0;
-
-        score0 = neloToScorePenta(elo0_, variance);
-        score1 = neloToScorePenta(elo1_, variance);
+        double t0 = std::sqrt(2.0) * elo0_ / (800.0 / std::log(10));
+        double t1 = std::sqrt(2.0) * elo1_ / (800.0 / std::log(10));
+        return getLLR_normalized(total, {0.0, 0.25, 0.5, 0.75, 1.0}, probs, t0, t1);
     } else {
-        score0 = leloToScore(elo0_);
-        score1 = leloToScore(elo1_);
+        double score0 = leloToScore(elo0_);
+        double score1 = leloToScore(elo1_);
+        return getLLR_logistic(total, {0.0, 0.25, 0.5, 0.75, 1.0}, probs, score0, score1);
+    }
+}
+
+template <size_t N>
+static double mean(std::array<double, N> x, std::array<double, N> p) {
+    double result = 0.0;
+    for (size_t i = 0; i < N; i++) result += x[i] * p[i];
+    return result;
+}
+
+template <size_t N>
+static std::tuple<double, double> mean_and_variance(std::array<double, N> x, std::array<double, N> p) {
+    double mu  = mean(x, p);
+    double var = 0.0;
+    for (size_t i = 0; i < N; i++) var += p[i] * (x[i] - mu) * (x[i] - mu);
+    return std::make_tuple(mu, var);
+}
+
+// I. F. D. Oliveira and R. H. C. Takahashi. 2020. An Enhancement of the Bisection Method Average Performance
+// Preserving Minmax Optimality. ACM Trans. Math. Softw. 47, 1, Article 5 (March 2021).
+// https://doi.org/10.1145/3423597
+template <typename F>
+static double itp(F f, double a, double b, double k_1, double k_2, double n_0, double epsilon) {
+    double f_a = f(a);
+    double f_b = f(b);
+
+    if (f_a > 0) {
+        std::swap(a, b);
+        std::swap(f_a, f_b);
+    }
+    assert(f_a < 0 && 0 < f_b);
+
+    double n_half = std::ceil(std::log2(std::abs(b - a) / (2.0 * epsilon)));
+    double n_max  = n_half + n_0;
+    for (size_t i = 0; std::abs(b - a) > 2.0 * epsilon; i++) {
+        double x_half = (a + b) / 2.0;
+        double r      = epsilon * std::pow(2.0, n_max - i) - (b - a) / 2.0;
+        double delta  = k_1 * std::pow(b - a, k_2);
+
+        double x_f = (f_b * a - f_a * b) / (f_b - f_a);
+
+        double sigma = (x_half - x_f) / std::abs(x_half - x_f);
+        double x_t   = delta <= std::abs(x_half - x_f) ? x_f + sigma * delta : x_half;
+
+        double x_itp = std::abs(x_t - x_half) <= r ? x_t : x_half - sigma * r;
+
+        double f_itp = f(x_itp);
+        if (std::fpclassify(f_itp) == FP_ZERO) {
+            a = x_itp;
+            b = x_itp;
+        } else if (std::signbit(f_itp)) {
+            a   = x_itp;
+            f_a = f_itp;
+        } else {
+            b   = x_itp;
+            f_b = f_itp;
+        }
     }
 
-    const double WW_dev0   = WW * std::pow((1 - score0), 2);
-    const double WD_dev0   = WD * std::pow((0.75 - score0), 2);
-    const double WLDD_dev0 = (WL + DD) * std::pow((0.5 - score0), 2);
-    const double LD_dev0   = LD * std::pow((0.25 - score0), 2);
-    const double LL_dev0   = LL * std::pow((0 - score0), 2);
-    const double variance0 = WW_dev0 + WD_dev0 + WLDD_dev0 + LD_dev0 + LL_dev0;
-    const double WW_dev1   = WW * std::pow((1 - score1), 2);
-    const double WD_dev1   = WD * std::pow((0.75 - score1), 2);
-    const double WLDD_dev1 = (WL + DD) * std::pow((0.5 - score1), 2);
-    const double LD_dev1   = LD * std::pow((0.25 - score1), 2);
-    const double LL_dev1   = LL * std::pow((0 - score1), 2);
-    const double variance1 = WW_dev1 + WD_dev1 + WLDD_dev1 + LD_dev1 + LL_dev1;
+    return (a + b) / 2.0;
+}
 
-    if (variance0 == 0 || variance1 == 0) return 0.0;
+template <size_t N>
+double SPRT::getLLR_logistic(double total, std::array<double, N> scores, std::array<double, N> probs, double s0,
+                             double s1) const noexcept {
+    // Compute the maximum likelihood estimate for a discrete probability distribution with an expectation of s,
+    // given an empirical distribution. See proposition 1.1 of [1] for details.
+    //
+    // [1]: Michel Van den Bergh, Comments on Normalized Elo,
+    // https://www.cantate.be/Fishtest/normalized_elo_practical.pdf
+    const auto mle = [&](double s) -> std::array<double, N> {
+        const double theta_epsilon = 1e-3;
 
-    // For more information: http://hardy.uhasselt.be/Fishtest/support_MLE_multinomial.pdf
-    return 0.5 * pairs * std::log(variance0 / variance1);
+        // Solve equation 1.3 in [1] for theta.
+        double min_theta = -1.0 / (scores[N - 1] - s);
+        double max_theta = -1.0 / (scores[0] - s);
+        double theta     = itp(
+            [&](double x) -> double {
+                double result = 0.0;
+                for (size_t i = 0; i < N; i++) {
+                    double a_i    = scores[i];
+                    double phat_i = probs[i];
+                    result += phat_i * (a_i - s) / (1.0 + x * (a_i - s));
+                }
+                return result;
+            },
+            min_theta, max_theta, 0.1, 2.0, 0.99, theta_epsilon);
+
+        // ML distribution is given by equation 1.2 in [1].
+        std::array<double, N> p;
+        for (size_t i = 0; i < N; i++) {
+            double a_i    = scores[i];
+            double phat_i = probs[i];
+            p[i]          = phat_i / (1 + theta * (a_i - s));
+        }
+        return p;
+    };
+
+    std::array<double, N> p0 = mle(s0);
+    std::array<double, N> p1 = mle(s1);
+    std::array<double, N> lpr;
+    for (size_t i = 0; i < N; i++) lpr[i] = std::log(p1[i]) - std::log(p0[i]);
+    return total * mean(lpr, probs);
+}
+
+template <size_t N>
+double SPRT::getLLR_normalized(double total, std::array<double, N> scores, std::array<double, N> probs, double t0,
+                               double t1) const noexcept {
+    // Compute the maximum likelihood estimate for a discrete probability distribution that has t = (mu - mu_ref) /
+    // sigma, given an empirical distribution. See section 4.1 of [1] for details.
+    //
+    // [1]: Michel Van den Bergh, Comments on Normalized Elo,
+    // https://www.cantate.be/Fishtest/normalized_elo_practical.pdf
+    const auto mle = [&](double mu_ref, double t_star) -> std::array<double, N> {
+        const double theta_epsilon = 1e-7;
+        const double mle_epsilon   = 1e-4;
+
+        // This is an iterative method, so we need to start with an initial value. As suggested in [1], we start with a
+        // uniform distribution.
+        std::array<double, N> p;
+        std::fill(p.begin(), p.end(), 1.0 / N);
+
+        while (true) {
+            // Calculate phi.
+            auto [mu, var] = mean_and_variance(scores, p);
+            std::array<double, N> phi;
+            for (size_t i = 0; i < N; i++) {
+                double a_i   = scores[i];
+                double sigma = std::sqrt(var);
+                phi[i] = a_i - mu_ref - 0.5 * t_star * sigma * (1.0 + ((a_i - mu) / sigma) * ((a_i - mu) / sigma));
+            }
+
+            // We need to find a subset of the possible solutions for theta,
+            // so we need to calculate our constraints for theta.
+            double u         = *std::min_element(phi.begin(), phi.end());
+            double v         = *std::max_element(phi.begin(), phi.end());
+            double min_theta = -1.0 / v;
+            double max_theta = -1.0 / u;
+
+            // Solve equation 4.9 in [1] for theta.
+            double theta = itp(
+                [&](double x) -> double {
+                    double result = 0.0;
+                    for (size_t i = 0; i < N; i++) {
+                        double phat_i = probs[i];
+                        result += phat_i * phi[i] / (1.0 + x * phi[i]);
+                    }
+                    return result;
+                },
+                min_theta, max_theta, 0.1, 2.0, 0.99, theta_epsilon);
+
+            double max_diff = 0.0;
+            // Calculate new estimate
+            for (size_t i = 0; i < N; i++) {
+                double phat_i = probs[i];
+                double newp_i = phat_i / (1.0 + theta * phi[i]);
+                max_diff      = std::max(max_diff, std::abs(newp_i - p[i]));
+                p[i]          = newp_i;
+            }
+
+            // Good enough?
+            if (max_diff < mle_epsilon) break;
+        }
+
+        return p;
+    };
+
+    std::array<double, N> p0 = mle(0.5, t0);
+    std::array<double, N> p1 = mle(0.5, t1);
+    std::array<double, N> lpr;
+    for (size_t i = 0; i < N; i++) lpr[i] = std::log(p1[i]) - std::log(p0[i]);
+    return total * mean(lpr, probs);
 }
 
 SPRTResult SPRT::getResult(double llr) const noexcept {

ddfe99c5264907b7d623e9efa661f4972e1d6808
Disservin <disservin.social@gmail.com>
2025-02-16 11:14:15 +0100

    fix: directory name in makefile

--- app/configuration/spawn-check.mk
+++ app/configuration/spawn-check.mk
@@ -1,9 +1,9 @@
 HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR := $(shell \
-    if $(CXX) ./compatability/spawn.c -o test 2>/dev/null; then echo 1; else echo 0; fi; \
+    if $(CXX) ./configuration/spawn.c -o test 2>/dev/null; then echo 1; else echo 0; fi; \
     rm test)
 
 ifeq ($(HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR),0)
 HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR_NP := $(shell \
-    if $(CXX) ./compatability/spawn_np.c -o test 2>/dev/null; then echo 1; else echo 0; fi; \
+    if $(CXX) ./configuration/spawn_np.c -o test 2>/dev/null; then echo 1; else echo 0; fi; \
     rm test)
 endif

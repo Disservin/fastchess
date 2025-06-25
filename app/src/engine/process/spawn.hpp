8d8a36cbad7d6193b23b424d08554b66a00149ff
Disservin <disservin.social@gmail.com>
2025-06-25 21:46:21 +0200

    test

--- app/src/engine/process/spawn.hpp
+++ app/src/engine/process/spawn.hpp
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
@@ -16,14 +17,13 @@
 #ifndef __APPLE__
 extern char **environ;
 #else
-#include <crt_externs.h>
+#    include <crt_externs.h>
 #endif
 
 namespace fastchess::engine::process {
 
 inline void redirect_fd(int from_fd, int to_fd) {
     if (dup2(from_fd, to_fd) < 0) {
-        perror("dup2 failed");
         _exit(127);
     }
 }
@@ -33,74 +33,135 @@ inline void close_fd(int fd) { close(fd); }
 inline void change_working_dir(const std::string &wd) {
     if (!wd.empty()) {
         if (chdir(wd.c_str()) != 0) {
-            perror("chdir failed");
             _exit(127);
         }
     }
 }
 
-// 0 on success, non-zero on failure
+// Returns 0 on success, errno value on failure
 inline int spawn_process(const std::string &command, char *const argv[], const std::string &working_dir, int stdin_fd,
                          int stdout_fd, int stderr_fd, pid_t &out_pid) {
     int status_pipe[2];
-    if (pipe(status_pipe) != 0) {
-        throw std::runtime_error("pipe failed");
+    int ec = 0;
+
+#ifdef O_CLOEXEC
+    if (pipe2(status_pipe, O_CLOEXEC) != 0) {
+        return errno;
     }
+#else
+    if (pipe(status_pipe) != 0) {
+        return errno;
+    }
+
+    fcntl(status_pipe[0], F_SETFD, FD_CLOEXEC);
+    fcntl(status_pipe[1], F_SETFD, FD_CLOEXEC);
+#endif
+
+    sigset_t oldmask, blockset;
+    sigfillset(&blockset);
+    sigprocmask(SIG_BLOCK, &blockset, &oldmask);
 
     pid_t pid = fork();
     if (pid < 0) {
+        ec = errno;
         close(status_pipe[0]);
         close(status_pipe[1]);
-        throw std::runtime_error("fork failed");
+        sigprocmask(SIG_SETMASK, &oldmask, nullptr);
+        return ec;
     }
 
     if (pid == 0) {
         // --- Child ---
-        // close read end
+        int ret = 0;
+        int p   = status_pipe[1];
+
         close(status_pipe[0]);
 
-        // Set pipe to close on exec
-        fcntl(status_pipe[1], F_SETFD, FD_CLOEXEC);
+        struct sigaction sa = {};
+        sa.sa_handler       = SIG_DFL;
+        for (int i = 1; i < NSIG; i++) {
+            sigaction(i, &sa, nullptr);
+        }
 
-        // File actions
-        redirect_fd(stdin_fd, STDIN_FILENO);
-        redirect_fd(stdout_fd, STDOUT_FILENO);
-        redirect_fd(stderr_fd, STDERR_FILENO);
+        if (stdin_fd == p) {
+            ret = dup(p);
+            if (ret < 0) goto fail;
+            close(p);
+            p = ret;
+        }
 
-        change_working_dir(working_dir);
+        if (stdout_fd == p) {
+            ret = dup(p);
+            if (ret < 0) goto fail;
+            close(p);
+            p = ret;
+        }
+
+        if (stderr_fd == p) {
+            ret = dup(p);
+            if (ret < 0) goto fail;
+            close(p);
+            p = ret;
+        }
+
+        if ((ret = dup2(stdin_fd, STDIN_FILENO)) < 0) goto fail;
+        if ((ret = dup2(stdout_fd, STDOUT_FILENO)) < 0) goto fail;
+        if ((ret = dup2(stderr_fd, STDERR_FILENO)) < 0) goto fail;
+
+        if (!working_dir.empty()) {
+            if (chdir(working_dir.c_str()) != 0) {
+                ret = -errno;
+                goto fail;
+            }
+        }
+
+        fcntl(p, F_SETFD, FD_CLOEXEC);
+
+        sigprocmask(SIG_SETMASK, &oldmask, nullptr);
 
 #ifdef __APPLE__
-        char **&environ = *_NSGetEnviron();
-        // environ         = envp_arr;
         execvp(command.c_str(), argv);
 #else
         execvp(command.c_str(), argv);
 #endif
+        ret = -errno;
 
-        // If execvp failed
-        const char err = '1';
-        (void)!write(status_pipe[1], &err, 1);
+    fail:
+        ret = -ret;
+
+        if (ret)
+            while (write(p, &ret, sizeof(ret)) < 0);
+
         _exit(127);
     }
 
     // --- Parent ---
-    // close write end
     close(status_pipe[1]);
 
-    char result;
-    ssize_t n = read(status_pipe[0], &result, 1);
+    int child_errno = 0;
+    ssize_t n       = read(status_pipe[0], &child_errno, sizeof(child_errno));
+
+    if (pid > 0) {
+        if (n != sizeof(child_errno)) {
+            child_errno = 0;
+        } else {
+            waitpid(pid, nullptr, 0);
+            ec = child_errno;
+        }
+    } else {
+        ec = errno;
+    }
+
     close(status_pipe[0]);
 
-    if (n == 0) {
-        // success
+    sigprocmask(SIG_SETMASK, &oldmask, nullptr);
+
+    if (!ec) {
         out_pid = pid;
         return 0;
     }
 
-    // failure
-    // avoid zombie
-    // waitpid(pid, nullptr, 0);
-    return 127;
+    return ec;
 }
 
 }  // namespace fastchess::engine::process

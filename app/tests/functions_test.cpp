4a3d6cd6a774073b52f865eba2b04b5bfc72e804
Disservin <disservin.social@gmail.com>
2025-11-30 22:16:13 +0100

    fix: Out of Bounds Access when no bestmove is reported

--- app/tests/functions_test.cpp
+++ app/tests/functions_test.cpp
@@ -17,4 +17,10 @@ TEST_SUITE("Standalone Function Tests") {
         CHECK(str_utils::contains("-engine", "e"));
         CHECK(str_utils::contains("info string depth 10", "depth"));
     }
+
+    TEST_CASE("Testing the str_utils::findElement function") {
+        const auto str = "bestmove ";
+
+        CHECK(!str_utils::findElement<std::string>(str_utils::splitString(str, ' '), "bestmove").has_value());
+    }
 }

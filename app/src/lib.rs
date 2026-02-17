d85ba4614d5b7278527cfc94ecfb24dc2e32eea2
Disservin <disservin.social@gmail.com>
2026-02-17 22:34:02 +0100

    abstract in helper function

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -13,3 +13,19 @@ use std::sync::atomic::AtomicBool;
 pub static STOP: AtomicBool = AtomicBool::new(false);
 /// Set when the program terminates abnormally (engine crash without recover, etc.)
 pub static ABNORMAL_TERMINATION: AtomicBool = AtomicBool::new(false);
+
+pub fn set_stop() {
+    STOP.store(true, std::sync::atomic::Ordering::Relaxed);
+}
+
+pub fn is_stop() -> bool {
+    STOP.load(std::sync::atomic::Ordering::Relaxed)
+}
+
+pub fn set_abnormal_termination() {
+    ABNORMAL_TERMINATION.store(true, std::sync::atomic::Ordering::Relaxed);
+}
+
+pub fn is_abnormal_termination() -> bool {
+    ABNORMAL_TERMINATION.load(std::sync::atomic::Ordering::Relaxed)
+}

f15f44051632c9b519e468330c0705475a3dbe06
disservin <disservin.social@gmail.com>
2026-05-05 21:06:44 +0200

    refactor: Show Last Extracted Line (#1003)

--- app/src/engine/uci_engine.cpp
+++ app/src/engine/uci_engine.cpp
@@ -442,16 +442,18 @@ std::optional<std::string> UciEngine::bestmove() const {
     const auto& last_line = lines.back()->line;
 
     if (last_line.rfind("bestmove", 0) != 0) {
-        Logger::print<Logger::Level::WARN>("Warning; No bestmove found in the last line from {} because: {}",
-                                           config_.name, "Line does not start with 'bestmove'");
+        Logger::print<Logger::Level::WARN>(
+            "Warning; No bestmove found in the last line from {} because: {}. Last line was: {}", config_.name,
+            "Line does not start with 'bestmove'", last_line);
         return std::nullopt;
     }
 
     const auto bm = str_utils::findElement<std::string>(str_utils::splitString(last_line, ' '), "bestmove");
 
     if (!bm.has_value()) {
-        Logger::print<Logger::Level::WARN>("Warning; No bestmove found in the last line from {} because: {}",
-                                           config_.name, bm.error());
+        Logger::print<Logger::Level::WARN>(
+            "Warning; No bestmove found in the last line from {} because: {}. Last line was: {}", config_.name,
+            bm.error(), last_line);
         return std::nullopt;
     }
 

c8d35a1bf25ed5c25fdc78450c775b655ad64a47
Disservin <disservin.social@gmail.com>
2026-07-26 12:36:06 +0200

    update

--- app/src/types/match_data.hpp
+++ app/src/types/match_data.hpp
@@ -9,6 +9,7 @@
 
 #include <core/time/time.hpp>
 #include <matchmaking/game_pair.hpp>
+#include <matchmaking/player.hpp>
 #include <types/engine_config.hpp>
 #include <types/score.hpp>
 
@@ -61,12 +62,11 @@ enum class MatchTermination {
 struct MatchData {
     struct PlayerInfo {
         PlayerInfo() = default;
-        PlayerInfo(EngineConfiguration engine_config, chess::GameResult game_result, std::string name = {},
-                   std::string author = {})
-            : config(std::move(engine_config)),
-              result(game_result),
-              uci_name(std::move(name)),
-              uci_author(std::move(author)) {}
+        PlayerInfo(const Player& player)
+            : config(player.engine.getConfig()),
+              result(player.getResult()),
+              uci_name(player.engine.idName().value_or("")),
+              uci_author(player.engine.idAuthor().value_or("")) {}
 
         EngineConfiguration config;
         chess::GameResult result = chess::GameResult::NONE;
